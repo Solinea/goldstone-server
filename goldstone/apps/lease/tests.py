630b308822af9734a2e18246ac077e2b0879c6b9
Ken Pepple <ken@solinea.com>
2013-11-09 13:30:13 +0800

    first testing start

--- goldstone/apps/lease/tests.py
+++ goldstone/apps/lease/tests.py
@@ -1,3 +1,15 @@
+from django.core.urlresolvers import resolve 
 from django.test import TestCase
+from lease.views import home_page
 
-# Create your tests here.
+class LeaseTest(TestCase):
+
+    def setup():
+        pass
+
+    def teardown():
+        pass
+
+    def test_root_url_resolves_to_home_page_view(self):
+        found = resolve('/lease')
+        self.assertEqual(found.func, home_page)
