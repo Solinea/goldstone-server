a773510fbe19ff1dce3d5d4fd8ed531bc2048c4f
Ken Pepple <ken@solinea.com>
2013-11-09 13:30:28 +0800

    added bootstrap admin egg

--- goldstone/settings.py
+++ goldstone/settings.py
@@ -30,6 +30,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = (
+    'django_admin_bootstrapped',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
