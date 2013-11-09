9e18e6057371c8a32d51005c6b53752ef9aee9f6
Ken Pepple <ken@solinea.com>
2013-11-09 21:39:18 +0800

    first pass at lease model

--- goldstone/apps/lease/models.py
+++ goldstone/apps/lease/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
-# Create your models here.
+class Lease(models.Model):
+    name = models.CharField(max_length=100)
+    deleted = models.BooleanField()
+    scope = models.CharField(max_length=100)
+    lease_type = models.CharField(max_length=100)
+    resource_id = models.CharField(max_length=100)
+    resource_type = models.CharField(max_length=100)
+    tenant_id = models.CharField(max_length=100)
+    start_time = models.DateTimeField()
+    length_in_seconds = models.CharField(max_length=100)
+    status = models.CharField(max_length=100)
+    action_time = models.DateTimeField()
+    action = models.ForeignKey(Action)
+    action_metadata = models.CharField(max_length=100)
+    action_result = model.CharField(max_length=100)
+    notification = models.ForeignKey(Notification)
+    notification_metadata = models.CharField(max_length=100)
+    notification_time = models.DateTimeField()
+    notification_result = models.CharField(max_length=100)
+
+class Notification(models.Model):
+    name = models.CharField(max_length=100)
+    driver = models.CharField(max_length=100)
+
+class Action(models.Model):
+    name = models.CharField(max_length=100)
+    driver = models.CharField(max_length=100)
--- goldstone/apps/lease/tests.py
+++ goldstone/apps/lease/tests.py
@@ -4,10 +4,10 @@ from lease.views import home_page
 
 class LeaseTest(TestCase):
 
-    def setup():
+    def setUp(self):
         pass
 
-    def teardown():
+    def tearDown(self):
         pass
 
     def test_root_url_resolves_to_home_page_view(self):
