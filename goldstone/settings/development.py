a3219c45d376aecbd8fce696dd90593517dc924f
Ken Pepple <ken@solinea.com>
2013-11-09 17:52:10 +0800

    base multi-setting files

---
+++ goldstone/settings/development.py
@@ -0,0 +1,24 @@
+# settings/development.py
+from .base import *
+
+DEBUG = True
+TEMPLATE_DEBUG = DEBUG
+
+EMAIL_HOST = "localhost"
+EMAIL_PORT = 1025
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": "goldstone",
+        "USER": "goldstone",
+        "PASSWORD": "",
+        "HOST": "localhost",
+        "PORT": "",
+    }
+
+INSTALLED_APPS += ("debug_toolbar", )
+
+INTERNAL_IPS = ("127.0.0.1")
+
+# MIDDLEWARE_CLASSES += ("debug_toolbar.middleware.DebugToolbarMiddleware", )
