3891986f778c86f42749622ce0f695f74f81dd0d
Ken Pepple <ken@solinea.com>
2013-11-10 18:40:15 +0800

    made notifications and actions many:1 with leases

--- goldstone/apps/lease/models.py
+++ goldstone/apps/lease/models.py
@@ -1,15 +1,8 @@
 from django.db import models
 
-class Notification(models.Model):
-    name = models.CharField(max_length=100)
-    driver = models.CharField(max_length=100)
-
-class Action(models.Model):
-    name = models.CharField(max_length=100)
-    driver = models.CharField(max_length=100)
-
 class Lease(models.Model):
     name = models.CharField(max_length=100)
+    owner_id = models.CharField(max_length=100)
     deleted = models.BooleanField()
     scope = models.CharField(max_length=100)
     lease_type = models.CharField(max_length=100)
@@ -19,11 +12,19 @@ class Lease(models.Model):
     start_time = models.DateTimeField()
     length_in_seconds = models.CharField(max_length=100)
     status = models.CharField(max_length=100)
-    action_time = models.DateTimeField()
-    action = models.ForeignKey(Action)
-    action_metadata = models.CharField(max_length=100)
-    action_result = models.CharField(max_length=100)
-    notification = models.ForeignKey(Notification)
-    notification_metadata = models.CharField(max_length=100)
-    notification_time = models.DateTimeField()
-    notification_result = models.CharField(max_length=100)
+
+class Notification(models.Model):
+    name = models.CharField(max_length=100)
+    driver = models.CharField(max_length=100)
+    metadata = models.CharField(max_length=100)
+    time = models.DateTimeField()
+    result = models.CharField(max_length=100)
+    lease = models.ForeignKey(Lease)
+
+class Action(models.Model):
+    name = models.CharField(max_length=100)
+    driver = models.CharField(max_length=100)
+    metadata = models.CharField(max_length=100)
+    time = models.DateTimeField()
+    result = models.CharField(max_length=100)
+    lease = models.ForeignKey(Lease)
