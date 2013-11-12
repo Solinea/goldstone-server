57e8ec5f96a2853596f4b756d297d7caadecfc3e
Ken Pepple <ken@solinea.com>
2013-11-12 22:29:35 +0900

    only support 2.7+

--- goldstone/apps/lease/views.py
+++ goldstone/apps/lease/views.py
@@ -1,5 +1,32 @@
 from django.shortcuts import render
 
 
-def home():
+def list_leases():
+    """
+    list all valid leases
+    """
+    pass
+
+def create_lease():
+    """
+    create a new lease
+    """
+    pass
+
+def update_lease():
+    """
+    update an existing lease
+    """
+    pass
+
+def delete_lease():
+    """
+    delete a lease
+    """
+    pass
+
+def expiring_leases(tolerance=300):
+    """
+    Find leases expiring
+    """
     pass
