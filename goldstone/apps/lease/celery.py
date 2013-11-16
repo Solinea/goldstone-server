8a2a32c36b282b80728e4976a69b4f99a8ae83e5
Ken Pepple <ken@solinea.com>
2013-11-16 09:35:54 -0800

    added basic celery support for leases

---
+++ goldstone/apps/lease/celery.py
@@ -0,0 +1,11 @@
+from __future__ import absolute_import
+from celery import Celery
+
+app = Celery(’lease.celery', broker='', backend=’', include=[’lease.tasks’])
+
+app.conf.update(
+    CELERY_TASK_RESULT_EXPIRES=3600,
+)
+
+if __name__ == ’__main__’:
+    app.start()
