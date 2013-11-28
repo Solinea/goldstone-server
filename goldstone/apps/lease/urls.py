f4d34210c1ccae7354bb6d0a3a31e3e504f35891
Ken Pepple <ken@solinea.com>
2013-11-28 11:28:16 -0500

    fixed remaining lease urls

--- goldstone/apps/lease/urls.py
+++ goldstone/apps/lease/urls.py
@@ -1,14 +1,15 @@
 from django.conf.urls import patterns, include, url
 
-from .views import *
+from .views import ListLeaseView, CreateLeaseView
+from .views import UpdateLeaseView, UpdateLeaseView
 
 urlpatterns = patterns(
-    'apps.lease.views',
+    '',
     url(r'^$',  ListLeaseView.as_view(), name='lease-list'),
     url(r'^/new$', CreateLeaseView.as_view(),
         name='lease-new',),
     url(r'^/edit/(?P<pk>\d+)/$', UpdateLeaseView.as_view(),
         name='lease-edit',),
-    url(r'^/delete/(?P<pk>\d+)/$', DeleteLeaseView.as_view(),
+    url(r'^/delete/(?P<pk>\d+)/$', UpdateLeaseView.as_view(),
         name='lease-delete',),
 )
