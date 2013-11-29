8a1c43de63086c87bbeafcf99b793477f2585860
Ken Pepple <ken@solinea.com>
2013-11-28 21:43:47 -0500

    fixed double / in urls

--- goldstone/apps/lease/urls.py
+++ goldstone/apps/lease/urls.py
@@ -6,10 +6,10 @@ from .views import UpdateLeaseView, UpdateLeaseView
 urlpatterns = patterns(
     '',
     url(r'^$',  ListLeaseView.as_view(), name='lease-list'),
-    url(r'^/new$', CreateLeaseView.as_view(),
+    url(r'^new$', CreateLeaseView.as_view(),
         name='lease-new',),
-    url(r'^/edit/(?P<pk>\d+)/$', UpdateLeaseView.as_view(),
+    url(r'^edit/(?P<pk>\d+)/$', UpdateLeaseView.as_view(),
         name='lease-edit',),
-    url(r'^/delete/(?P<pk>\d+)/$', UpdateLeaseView.as_view(),
+    url(r'^delete/(?P<pk>\d+)/$', UpdateLeaseView.as_view(),
         name='lease-delete',),
 )
