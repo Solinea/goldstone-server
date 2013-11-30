ef97bfba569f7e564c7f4ee6fbd7687b202999ad
Ken Pepple <ken@solinea.com>
2013-11-30 01:33:40 -0500

    reorganized celery conf

--- goldstone/settings/development.py
+++ goldstone/settings/development.py
@@ -17,11 +17,19 @@ DATABASES = {
 
 SECRET_KEY = 'dev-v=jazz^xno*0(aou-6ir*q-c+v&r#ue5b4wxt-xy#rebph8q)'
 
+# OpenStack Admin Configuration (TODO: remove)
+
+OS_PASSWORD = "password"
+OS_AUTH_URL = "http://10.11.115.20:5000/v2.0"
+OS_USERNAME = "admin"
+OS_TENANT_NAME = "admin"
+
 # Celery configuration
 
-BROKER_URL = 'redis://'
+# BROKER_URL = 'redis://'
+BROKER_URL = 'sqla+postgresql://goldstone:@localhost/goldstone_dev'
 
-CELERY_TIMEZONE = 'UTC'
+# CELERY_TIMEZONE = 'UTC'
 
 # CELERYBEAT_SCHEDULE = {'expiring_leases': {
 #     'task': 'tasks.pull_expiring_leases',
