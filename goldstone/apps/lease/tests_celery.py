bb36056fd2465451b539588bdebc694cf8462c8e
Ken Pepple <ken@solinea.com>
2013-11-30 02:08:16 -0500

    celery test setup

--- goldstone/apps/lease/tests_celery.py
+++ goldstone/apps/lease/tests_celery.py
@@ -1,17 +1,39 @@
 from django.test import TestCase
-# from apps.lease.celery import app
+from django.utils import timezone
+
+from .models import Lease, Notification, Action
+# from .tasks import notify, expire
 
 
 class CeleryLeaseTest(TestCase):
 
     def setUp(self):
+        # create three leases, each with a notification and action
+        lease = Lease(
+            name='Lease 1',
+            deleted=False,
+            start_time=timezone.now(),
+        )
+        lease.save()
+        lease.notification_set.create(
+            name="samples notification",
+            driver="email",
+            time=timezone.now(),
+            result="success",
+            )
+        lease.action_set.create(
+            name="sample action",
+            driver="terminate",
+            time=timezone.now(),
+            result="pending",
+            )
         pass
 
     def tearDown(self):
         pass
 
-    def test_pull_notifications(self):
+    def test_notifications(self):
         pass
 
-    def test_pull_expirations(self):
+    def test_expires(self):
         pass
--- goldstone/celery.py
+++ goldstone/celery.py
@@ -2,10 +2,10 @@ from celery import Celery
 from django.conf import settings
 
 app = Celery('goldstone.celery')
-app.config_from_object('django.conf:settings') 
+app.config_from_object('django.conf:settings')
 app.autodiscover_tasks(settings.INSTALLED_APPS, related_name='tasks')
 
 
-@app.task(bind=True) 
+@app.task(bind=True)
 def debug_task(self):
     print('Request: {0!r}'.format(self.request))
