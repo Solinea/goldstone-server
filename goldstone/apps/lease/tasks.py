aa6b706c5d2c58a9a167d0fad5e8eb5a5582bb05
Ken Pepple <ken@solinea.com>
2013-11-30 01:33:28 -0500

    simplified tasks to just expire and notify

--- goldstone/apps/lease/tasks.py
+++ goldstone/apps/lease/tasks.py
@@ -1,61 +1,58 @@
-from __future__ import absolute_import
-from proj.celery import app
-
+from datetime import datetime, timedelta
 import os
-from datetime import timedelta
 
-ACTION_OFFSET = 120
-NOTIFICATION_OFFSET = 120
-QUERY_OFFSET = 60 * 10
+from celery import Celery
+from django.conf import settings
 
-CELERY_TIMEZONE = 'UTC'
-
-CELERYBEAT_SCHEDULE = {'expiring_leases': {
-    'task': 'tasks.pull_expiring_leases',
-    'schedule': timedelta(seconds=QUERY_OFFSET),
-    'args': '',
-    }, 'upcoming_notifications': {
-    'task': 'tasks.pull_upcoming_notifications',
-    'schedule': timedelta(seconds=QUERY_OFFSET),
-    'args': '',
-    },
-}
+from .models import Lease, Action, Notification
 
 
-def _get_admin_keys():
+app = Celery('goldstone.celery')
+
+
+def _get_admin_creds():
     """
     Get admin keys to execute privileged OpenStack actions
     """
+    # TODO: fake this until keystone integration is done
+    return {"OS_PASSWORD": settings.OS_PASSWORD,
+            "OS_AUTH_URL": settings.OS_AUTH_URL,
+            "OS_USERNAME": settings.OS_USERNAME,
+            "OS_TENANT_NAME": settings.OS_TENANT_NAME,
+            }
+
+
+@app.task
+def expire():
+    """
+    Expire leases
+    """
+    creds = _get_admin_creds()
+    expiring_leases = Action.objects.filter(status__iexact="pending")
+    expiring_leases = expiring_leases.objects.filter(time__lte=timezone.now())
+    for x in expiring_leases:
+        # determine action, scope and type of lease
+        # x.lease.scope and x.lease.lease_type
+        # update database status to "in action" or some shit
+        # expire lease w/ openstack cli
+        # wait for response
+        # updated database status to "completed" or some shit
+        pass
     pass
 
 
 @app.task
-def get_expiring_leases(offset=NOTIFICATION_OFFSET):
-    """
-    Query the database for leases expirations
-    """
-    pass
-
-
-@app.task
-def get_upcoming_notifications(offset=ACTION_OFFSET):
-    """
-    Query the database for upcoming notifications
-    """
-    pass
-
-
-@app.task
-def notify_expiring(lease, notification):
+def notify():
     """
     Send notifications
     """
-    pass
-
-
-@app.task
-def enforce_expiration(lease, action):
-    """
-    Enforce lease expirations
-    """
+    expiring_notifcations = Notification.objects.filter(
+        status__iexact="pending")
+    expiring_notifcations = expiring_notifcations.objects.filter(
+        time__lte=timezone.now())
+    for x in expiring:
+        # perform notification
+        # wait for response
+        # updated database status
+        pass
     pass
