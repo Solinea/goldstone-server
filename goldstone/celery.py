74d47004659c6ada73e9ed11c0aba8caf85bcbe9
Ken Pepple <ken@solinea.com>
2013-11-30 22:30:24 -0800

    followed https://github.com/celery/celery/tree/3.1/examples/django almost exactly to get celery working

--- goldstone/celery.py
+++ goldstone/celery.py
@@ -1,15 +1,23 @@
-# from __future__ import absolute_import
-# 
-# from celery import Celery
-# from django.conf import settings
-# 
-# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'conf.settings')
-# 
-# app = Celery('goldstone.apps.lease.celery', broker = 'redis://localhost:6379/0',)
-# app.config_from_object('django.conf:settings')
+from __future__ import absolute_import
+
+from celery import Celery
+from django.conf import settings
+
+# set the default Django settings module for the 'celery' program.
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'goldstone.settings.development')
+
+
+app = Celery('goldstone')
+app.config_from_object('django.conf:settings')
+app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
 # app.autodiscover_tasks(settings.INSTALLED_APPS, related_name='tasks')
-# 
-# 
-# @app.task(bind=True)
-# def debug_task(self):
-#     print('Request: {0!r}'.format(self.request))
+
+
+@app.task(bind=True)
+def debug_task(self):
+    print('Request: {0!r}'.format(self.request))
+
+
+@app.task(bind=True)
+def debug_task_example(self):
+    print(repr(self.request))
