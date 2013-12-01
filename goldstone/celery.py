d2b1d1ac4257bcad36af0c09899fb59f38163c74
Ken Pepple <ken@solinea.com>
2013-11-30 22:43:19 -0800

    pep8 cleanup

--- goldstone/celery.py
+++ goldstone/celery.py
@@ -3,14 +3,10 @@ from __future__ import absolute_import
 from celery import Celery
 from django.conf import settings
 
-# set the default Django settings module for the 'celery' program.
-# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'goldstone.settings.development')
-
 
 app = Celery('goldstone')
 app.config_from_object('django.conf:settings')
 app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
-# app.autodiscover_tasks(settings.INSTALLED_APPS, related_name='tasks')
 
 
 @app.task(bind=True)
