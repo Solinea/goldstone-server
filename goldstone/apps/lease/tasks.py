ec3d339a3dfc40b86ff5f89b89d7824941f3865b
Ken Pepple <ken@solinea.com>
2013-12-03 21:36:42 -0800

    framed tasks with loggers

--- goldstone/apps/lease/tasks.py
+++ goldstone/apps/lease/tasks.py
@@ -2,8 +2,14 @@ from datetime import datetime, timedelta
 import os
 
 from celery import task, shared_task
+from celery.utils.log import get_task_logger
+from django.utils import timezone
+from django.conf import settings
 
 from .models import Lease, Action, Notification
+
+
+logger = get_task_logger(__name__)
 
 
 def _get_admin_creds():
@@ -18,11 +24,18 @@ def _get_admin_creds():
             }
 
 
-def _compute_exists(instance_id):
+def _resource_exists(resource_type, instance_id):
+    logger.info('%s compute instance exists' % instance_id)
     return True
 
 
-def _volume_exists(volume_id):
+def _terminate_tenant_instances(tenant_id):
+    logger.info('terminating tenant %s instances' % tenant_id)
+    return True
+
+
+def _terminate_specific_instance(instance_id):
+    logger.info('terminating instance %s' % instance_id)
     return True
 
 
@@ -31,11 +44,24 @@ def expire(lease_id):
     """
     Expire leases
     """
+    logger.info('expire starting for lease %s' % lease_id)
     creds = _get_admin_creds()
     try:
-        lease_to_be_expired = Lease.get(lease_id)
+        expired_lease = Lease.get(lease_id)
     except:
-        pass
+        logger.warn("lease id %s does not exist in the database" % lease_id)
+        return False
+    if expired_lease.scope == "TENANT":
+        expire_result = _terminate_tenant_instances(expired_lease.tenant_id)
+    elif expired_lease.scope == "RESOURCE":
+        expire_result = _terminate_specific_instance(expired_lease.resource_id)
+    else:
+        logger.warn("lease %s has incorrect scope: %s" %
+                 (lease_id, expired_lease.scope))
+    if expire_result:
+        expired_lease.result = "COMPLETED"
+    else:
+        logger.warn('lease %s did not terminate' % lease_id)
     # check state to determine that it has been already expired
     # determine action, scope and type of lease
     # x.lease.scope and x.lease.lease_type
@@ -51,21 +77,33 @@ def find_expirations():
     """
     Query database for expired leases
     """
-    expired_leases = Action.objects.filter(status__iexact="pending")
-    expired_leases = expiring_leases.objects.filter(time__lte=timezone.now())
+    logger.info("finding expirations")
+    expired_leases = Action.objects.filter(result__iexact="pending")
+    expired_leases = expired_leases.filter(time__lte=timezone.now())
     for lease in expired_leases:
-        expire.delay(lease.id)
+        expire.delay(lease.pk)
+    return True
+
+
+def _send_notification(tenant, message):
+    return True
 
 
 @shared_task
-def notify():
+def notify(notification_id):
     """
     Send notifications
     """
-    # perform notification
-    # wait for response
-    # updated database status
-    return True
+    logger.info('notify starting')
+    try:
+        notification = Notification.get(notification_id)
+        days_left = timezone.now() - notification.lease.expiration_time
+        message = ("Your lease will expire in %s days." % days_left)
+        result = _send_notification(tenant, message)
+    except:
+        result = False
+        logger.warn("Failed to send notification %s" % notification_id)
+    return result
 
 
 @shared_task
@@ -73,9 +111,11 @@ def find_notifications():
     """
     Query database for expired leases
     """
+    logger.info('finding notifications')
     expiring_notifications = Notification.objects.filter(
-        status__iexact="pending")
-    expiring_notifications = expiring_notifcations.objects.filter(
+        result__iexact="pending")
+    expiring_notifications = expiring_notifications.filter(
         time__lte=timezone.now())
     for notification in expiring_notifications:
         notify.delay(notification.id)
+    return True
