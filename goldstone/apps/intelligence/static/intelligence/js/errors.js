43885f65a3b7e1822863d77e557705f674a14f29
John Stanford <john@solinea.com>
2013-12-23 08:08:19 -0800

    GOLD-114 placed static custom dashboards

--- goldstone/apps/intelligence/static/intelligence/js/errors.js
+++ goldstone/apps/intelligence/static/intelligence/js/errors.js
@@ -1,5 +1,6 @@
 $(document).ready(
     function() {
-        window.open('/static/intelligence/kibana/index.html#/dashboard/elasticsearch/Goldstone%20Log%20Errors');
+        /// window.open('/static/intelligence/kibana/index.html#/dashboard/elasticsearch/Goldstone%20Log%20Errors');
+        window.open('/static/intelligence/kibana/index.html#/dashboard/file/goldstone_errors.json');
     }
 );
--- goldstone/apps/intelligence/static/intelligence/js/search.js
+++ goldstone/apps/intelligence/static/intelligence/js/search.js
@@ -1,5 +1,6 @@
 $(document).ready(
     function() {
-        window.open('/static/intelligence/kibana/index.html');
+        ///window.open('/static/intelligence/kibana/index.html');
+        window.open('/static/intelligence/kibana/index.html#/dashboard/file/goldstone_search.json');
     }
 );
