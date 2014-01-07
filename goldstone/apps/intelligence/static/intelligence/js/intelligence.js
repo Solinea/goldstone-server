ab8f4110fccbf0a263dd68dbc5169a40ea4ec21d
John Stanford <john@solinea.com>
2014-01-06 18:10:00 -0800

    The first rendering of log data!

--- goldstone/apps/intelligence/static/intelligence/js/intelligence.js
+++ goldstone/apps/intelligence/static/intelligence/js/intelligence.js
@@ -8,37 +8,54 @@ height = 300 - margin.top - margin.bottom;
 
 function draw_cockpit_panel(view_data) {
 
-    alert("in draw_cockpit_panel");
-    alert("xdata=" + view_data.xdata);
-
-    //console.log("initial width = " + panelWidth)
-    //console.log("initial height = " + panelHeight)
-
     var chart = dc.heatMap("#d3-panel-body");
 
-    d3.csv(view_data.datafile, function(error, experiments) {
+    var log_data = view_data.data
 
-      var ndx = crossfilter(experiments),
-          runDim = ndx.dimension(function(d) { return [+d.Run, +d.Expt]; }),
-          runGroup = runDim.group().reduceSum(function(d) { return +d.Speed; });
+    alert("log_data[0] = time:"+log_data[0].time + " component:" + log_data[0].component + " type: " + log_data[0].type + " count:" + log_data[0].count)
+
+    var ndx = crossfilter(log_data)
+    var runDim = ndx.dimension(function(d){
+            return d.time;
+        });
+    var runGroup = runDim.group().reduceSum(function(d){
+                                return d.count;
+                            });
+
+    chart
+        .width(width)
+        .height(height)
+        .dimension(runDim)
+        .group(runGroup);
+
+    chart.render();
+
+ /*   d3.json(view_data.data, function(error, logs) {
+
+      var ndx = crossfilter(logs),
+          runDim = ndx.dimension(function(d) { return [+d.time, +d.component]; })
+          //runGroup = runDim.group().reduceSum(function(d) { return +d.Speed; })
+      ;
+
+
 
       chart
         .width(width)
         .height(height)
         .dimension(runDim)
-        .group(runGroup)
+        //.group(runGroup)
         .keyAccessor(function(d) { return +d.key[0]; })
         .valueAccessor(function(d) { return +d.key[1]; })
         .colorAccessor(function(d) { return +d.value; })
         .title(function(d) {
-            return "Run:   " + d.key[0] + "\n" +
-                   "Expt:  " + d.key[1] + "\n" +
-                   "Speed: " + (299000 + d.value) + " km/s";})
+            return "Time:   " + d.key[0] + "\n" +
+                   "Component:  " + d.key[1] + "\n" +
+                   "Count: " + d.value + " " + d.type;})
         .colors(["#ffffd9","#edf8b1","#c7e9b4","#7fcdbb","#41b6c4","#1d91c0","#225ea8","#253494","#081d58"])
         .calculateColorDomain();
 
       chart.render();
-    });
+    });*/
 }
 
 /*
