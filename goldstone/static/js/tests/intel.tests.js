0cc3355649594f11b45f70549bee3596de88f4fe
John Stanford <john@solinea.com>
2014-03-12 16:20:38 -0700

    WIP django.js added and basic QUnit test runs.  Need to work out the JS includes and write up some tests

---
+++ goldstone/static/js/tests/intel.tests.js
@@ -0,0 +1,11 @@
+/**
+ * Created by stanford on 3/12/14.
+ */
+
+test( "hello test", function() {
+  ok( 1 == "1", "Passed!" );
+});
+
+/*test( "hello test", function() {
+  ok( 2 == "1", "Passed!" );
+});*/
---
+++ goldstone/tests.py
@@ -0,0 +1,11 @@
+from djangojs.runners import JsTestCase, JsFileTestCase, JsTemplateTestCase
+from djangojs.runners import JasmineSuite, QUnitSuite
+
+
+class QUnitTests(QUnitSuite, JsTestCase):
+    title = 'My QUnit suite'
+    url_name = 'my_qunit_view'
+    #js_files = [
+    #    'intelligence/static/intelligence/js/*',
+    #]
+
