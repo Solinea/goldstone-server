27ff9b569a5ff1a974edd572d07006137d7eccc3
John Stanford <john@solinea.com>
2014-03-24 14:15:13 -0700

    WIP

---
+++ goldstone/apps/core/templatetags/custom_filters.py
@@ -0,0 +1,18 @@
+__author__ = 'stanford'
+from django import template
+from django.utils.html import escapejs
+from django.utils.safestring import mark_safe
+import json
+
+register = template.Library()
+
+@register.filter
+def to_js(value):
+    """
+    To use a python variable in JS, we call json.dumps to serialize as JSON
+    server-side and reconstruct using JSON.parse. The serialized string must be
+    escaped appropriately before dumping into the client-side code.
+    """
+    # separators is passed to remove whitespace in output
+    return mark_safe('JSON.parse("%s")' % escapejs(
+        json.dumps(value, separators=(',', ':'))))
---
+++ goldstone/apps/nova/static/js/nova.js
@@ -0,0 +1,5 @@
+/**
+ * Created by stanford on 3/23/14.
+ */
+
+
