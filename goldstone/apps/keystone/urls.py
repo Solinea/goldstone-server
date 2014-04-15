34a6a50be2e5f1c29f9e6cfa0a08ddc05071ac22
John Stanford <john@solinea.com>
2014-04-14 18:00:41 -0700

    model and view look ok so far.  quite a bit of refactoring for reuse. committing before diving into JS refactoring.

---
+++ goldstone/apps/keystone/urls.py
@@ -0,0 +1,16 @@
+# vim: tabstop=4 shiftwidth=4 softtabstop=4
+
+#
+# Copyright 2014 Solinea, Inc.
+#
+
+from django.conf.urls import patterns, url
+from .views import *
+
+urlpatterns = patterns(
+    '',
+    url(r'^discover[/]?$', DiscoverView.as_view(),
+        name='keystone-discover-view'),
+    url(r'^report[/]?$', ReportView.as_view(),
+        name='keystone-report-view'),
+)
