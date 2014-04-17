bff05377c1dffb02f0e547814766e08b8860788d
John Stanford <john@solinea.com>
2014-04-17 13:18:50 -0700

    cinder list volumes chart working

--- goldstone/apps/cinder/static/cinder/js/cinder.js
+++ goldstone/apps/cinder/static/cinder/js/cinder.js
@@ -1,11 +1,11 @@
 /**
  * Created by stanford on 4/14/14.
  */
-goldstone.namespace('keystone.report')
-goldstone.namespace('keystone.authApiPerf')
-goldstone.namespace('keystone.timeRange')
+goldstone.namespace('cinder.report')
+goldstone.namespace('cinder.volListApiPerf')
+goldstone.namespace('cinder.timeRange')
 
-goldstone.keystone.timeRange._url = function (ns, start, end, interval, render, path) {
+goldstone.cinder.timeRange._url = function (ns, start, end, interval, render, path) {
     "use strict";
     var gt = goldstone.time
     start = start ? gt.toPyTs(start) : gt.toPyTs(ns.start)
@@ -22,9 +22,9 @@ goldstone.keystone.timeRange._url = function (ns, start, end, interval, render, 
     return url
 }
 
-goldstone.keystone.authApiPerf.url = function (start, end, interval, render) {
+goldstone.cinder.volListApiPerf.url = function (start, end, interval, render) {
     "use strict";
-    var ns = goldstone.keystone.authApiPerf,
-        path = "/keystone/auth_api_perf"
-    return goldstone.keystone.timeRange._url(ns, start, end, interval, render, path)
+    var ns = goldstone.cinder.volListApiPerf,
+        path = "/cinder/vol_list_api_perf"
+    return goldstone.cinder.timeRange._url(ns, start, end, interval, render, path)
 }
