9ed8c61559f46367f82256e0f7bed3ebfbefef78
John Stanford <john@solinea.com>
2014-05-20 10:28:26 -0700

    Removed all references to metrize icons as well as the old nova zones code.  The old functionality was superceded by the discover functionality.

--- goldstone/apps/nova/urls.py
+++ goldstone/apps/nova/urls.py
@@ -35,12 +35,8 @@ urlpatterns = patterns(
         name='nova-hypervisor-mem'),
     url(r'^hypervisor/disk[/]?$', DiskView.as_view(),
         name='nova-hypervisor-disk'),
-    # TODO simplify stats url space by using the parameter presence to
-    # decide whether it is an instantaneous or range request
     url(r'^hypervisor/latest-stats[/]?$', LatestStatsView.as_view(),
         name='nova-hypervisor-latest-stats'),
-    url(r'^zones[/]?$', ZonesView.as_view(),
-        name='nova-zones'),
     url(r'^api_perf[/]?$', ApiPerfView.as_view(),
         name='nova-api-perf'),
 )
--- goldstone/wsgi.py
+++ goldstone/wsgi.py
@@ -24,7 +24,8 @@ https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
 """
 
 import os
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "goldstone.settings.production")
+os.environ.setdefault(
+    "DJANGO_SETTINGS_MODULE", "goldstone.settings.production")
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
