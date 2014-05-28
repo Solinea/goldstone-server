bd3a1046d013b1a9857af0c3bcce16cd2c645078
Ken Pepple <ken@solinea.com>
2014-05-28 13:29:38 -0700

    httpd append fun

--- install.sh
+++ install.sh
@@ -34,24 +34,25 @@ function pre_install_sanity() {
 
 function install_elasticsearch() {
     curl -XGET https://download.elasticsearch.org/elasticsearch/elasticsearch/elasticsearch-1.1.1.noarch.rpm > elasticsearch-1.1.1.noarch.rpm
-    yum install java-1.7.0-openjdk.x86_64
-    yum install gcc
-    yum install gcc-c++
-    yum install python-devel
-    yum install postgresql-server
-    yum install postgresql-devel
-    yum install libffi-devel
-    yum install openssl-devel
-    yum install httpd
-    yum install mod_wsgi
-    rpm -Uhv elasticsearch-1.1.1.noarch.rpm
+    yum install -y java-1.7.0-openjdk.x86_64
+    yum install -y gcc
+    yum install -y gcc-c++
+    yum install -y python-devel
+    yum install -y postgresql-server
+    yum install -y postgresql-devel
+    yum install -y libffi-devel
+    yum install -y openssl-devel
+    yum install -y httpd
+    yum install -y mod_wsgi
+    yum install -u redis
+    yum localinstall -y elasticsearch-1.1.1.noarch.rpm
     chkconfig --add elasticsearch
     service elasticsearch start
 }
 
 function install_logstash() {
     curl -XGET https://download.elasticsearch.org/logstash/logstash/packages/centos/logstash-1.4.0-1_c82dc09.noarch.rpm > logstash-1.4.0-1_c82dc09.noarch.rpm
-    rpm -Uhv logstash-1.4.0-1_c82dc09.noarch.rpm
+    yum localinstall -y logstash-1.4.0-1_c82dc09.noarch.rpm
     /opt/logstash/bin/plugin install contrib  # required for translate plugin
     cp goldstone/external/logstash/conf.d/* /etc/logstash/conf.d/
     cp goldstone/external/logstash/patterns/goldstone /opt/logstash/patterns/goldstone
@@ -68,18 +69,19 @@ function install_pg() {
 }
 
 function configure_goldstone() {
-    echo "WSGIPythonPath /opt/goldstone:/opt/goldstone/lib/python2.6/site-packages" >> /etc/httpd/conf
-    echo "<VirtualHost *:80>" >> /etc/httpd/conf
-    echo "ServerAdmin you@example.com" >> /etc/httpd/conf
+    hc='/etc/httpd/conf/httpd.conf'
+    echo "WSGIPythonPath /opt/goldstone:/opt/goldstone/lib/python2.6/site-packages" >> $hc
+    echo "<VirtualHost *:80>" >> $hc
+    echo "ServerAdmin you@example.com" >> $hc
     h=`hostname`
-    echo "ServerName ${h}" >> /etc/httpd/conf
-    echo "WSGIScriptAlias / /opt/goldstone/goldstone/wsgi.py" >> /etc/httpd/conf
-    echo "Alias /static/ /var/www/goldstone/static/" >> /etc/httpd/conf
-    echo "Alias /favicon.ico /var/www/goldstone/static/images/favicon.ico" >> /etc/httpd/conf
-    echo "<Location \"/static/\">" >> /etc/httpd/conf
-    echo "    Options -Indexes" >> /etc/httpd/conf
-    echo "</Location>" >> /etc/httpd/conf
-    echo "</VirtualHost>" >> /etc/httpd/conf
+    echo "ServerName ${h}" >> $hc
+    echo "WSGIScriptAlias / /opt/goldstone/goldstone/wsgi.py" >> $hc
+    echo "Alias /static/ /var/www/goldstone/static/" >> $hc
+    echo "Alias /favicon.ico /var/www/goldstone/static/images/favicon.ico" >> $hc
+    echo "<Location \"/static/\">" >> $hc
+    echo "    Options -Indexes" >> $hc
+    echo "</Location>" >> $hc
+    echo "</VirtualHost>" >> $hc
     
     cp -r goldstone /opt/goldstone
     pip install -r requirements.txt
@@ -89,7 +91,7 @@ function configure_goldstone() {
     service httpd restart
 }
 
-pre_install_sanity
+# pre_install_sanity
 install_elasticsearch
 install_logstash
 install_pg
