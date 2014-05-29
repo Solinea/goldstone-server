ed4d48486262660530eaa1e31a13016a16475311
Ken Pepple <ken@solinea.com>
2014-05-29 16:45:33 -0700

    more minor installer changes

--- install_goldstone.sh
+++ install_goldstone.sh
@@ -3,21 +3,23 @@
 # ken@solinea.com
 # (c) Solinea, Inc  2014
 
-. ./install_functons.sh
+. ./install_functions.sh
+
 
 function report_status() {
     if [[ $result = 1 ]]; then
-        print "${stage}         [ DONE ]"
-    elif
-        print "${stage}         [ FAIL ]"
+        echo "${stage}         [ DONE ]"
+    else
+        echo "${stage}         [ FAIL ]"
     fi
 }
 
+logrunname="install.log"
 
-stage="EPEL"; result=$(setup_epel); report_status()
-stage="IPTABLES"; result=$(config_iptables); report_status()
-stage="ELASTICSEARCH"; result=$(install_elasticsearch); report_status()
-stage="LOGSTASH"; result=$(install_logstash); report_status()
-stage="POSTGRESQL"; result=$(install_pg); report_status()
-stage="GOLDSTONE"; result=$(configure_goldstone); report_status()
-stage="CELERY"; result=$(start_celery); report_status()
+stage="EPEL"; result=$(setup_epel 2>&1 $logrunname); report_status
+stage="IPTABLES"; result=$(config_iptables 2>&1 $logrunname); report_status
+stage="ELASTICSEARCH"; result=$(install_elasticsearch 2>&1 $logrunname); report_status
+stage="LOGSTASH"; result=$(install_logstash 2>&1 $logrunname); report_status
+stage="POSTGRESQL"; result=$(install_pg 2>&1 $logrunname); report_status
+stage="GOLDSTONE"; result=$(configure_goldstone 2>&1 $logrunname); report_status
+stage="CELERY"; result=$(start_celery 2>&1 $logrunname); report_status
