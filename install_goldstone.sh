2b8694c7b18bb1471aa734a1b41e64f791c87c13
Ken Pepple <ken@solinea.com>
2014-06-07 13:05:11 -0700

    added timestamp

--- install_goldstone.sh
+++ install_goldstone.sh
@@ -11,23 +11,28 @@ txtrst=$(tput sgr0) # reset
 
 function report_status() {
     d=`date`
-    if [ $result -eq 1 ]; then
+    if [ "$result" == "1" ]; then
         echo -e "${d}	${stage}         ${green_text}[ PASS ]${txtrst}"
     else
         echo -e "${d} 	${stage}         ${red_text}[ FAIL ]${txtrst}"
     fi
 }
 
+function datestamp() {
+    d=`date`
+    echo -e "${d} 	STARTING ${stage} ...."
+}
+
 logrunname="install.log"
 
-stage="EPEL"; result=$(setup_epel 2>&1 $logrunname); report_status
-stage="IPTABLES"; result=$(config_iptables 2>&1 $logrunname); report_status
-stage="ELASTICSEARCH"; result=$(install_elasticsearch 2>&1 $logrunname); report_status
-stage="LOGSTASH"; result=$(install_logstash 2>&1 $logrunname); report_status
-stage="LOGGING"; result=$(set_logging 2>&1 $logrunname); report_status
-stage="POSTGRESQL"; result=$(install_pg 2>&1 $logrunname); report_status
-stage="GOLDSTONE"; result=$(configure_goldstone 2>&1 $logrunname); report_status
-stage="CELERY"; result=$(start_celery 2>&1 $logrunname); report_status
+stage="EPEL"; datestamp; result=$(setup_epel 2>&1 $logrunname); report_status
+stage="IPTABLES"; datestamp; result=$(config_iptables 2>&1 $logrunname); report_status
+stage="ELASTICSEARCH"; datestamp; result=$(install_elasticsearch 2>&1 $logrunname); report_status
+stage="LOGSTASH"; datestamp; result=$(install_logstash 2>&1 $logrunname); report_status
+stage="LOGGING"; datestamp; result=$(set_logging 2>&1 $logrunname); report_status
+stage="POSTGRESQL"; datestamp; result=$(install_pg 2>&1 $logrunname); report_status
+stage="GOLDSTONE"; datestamp; result=$(configure_goldstone 2>&1 $logrunname); report_status
+stage="CELERY"; datestamp; result=$(start_celery 2>&1 $logrunname); report_status
 
 
 # setup_epel
