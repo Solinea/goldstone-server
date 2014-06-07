e79d383c170e178cc2ac29f8aba46af36d0204fc
Ken Pepple <ken@solinea.com>
2014-06-07 11:42:22 -0700

    small syntax changes

--- install_goldstone.sh
+++ install_goldstone.sh
@@ -23,7 +23,6 @@ stage="EPEL"; result=$(setup_epel 2>&1 $logrunname); report_status
 stage="IPTABLES"; result=$(config_iptables 2>&1 $logrunname); report_status
 stage="ELASTICSEARCH"; result=$(install_elasticsearch 2>&1 $logrunname); report_status
 stage="LOGSTASH"; result=$(install_logstash 2>&1 $logrunname); report_status
-<<<<<<< HEAD
 stage="LOGGING"; result=$(set_logging 2>&1 $logrunname); report_status
 stage="POSTGRESQL"; result=$(install_pg 2>&1 $logrunname); report_status
 stage="GOLDSTONE"; result=$(configure_goldstone 2>&1 $logrunname); report_status
