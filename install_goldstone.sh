8833f1a442e68d20856c00f0b03e482ba3cf63ba
Ken Pepple <ken@solinea.com>
2014-06-07 14:05:40 -0700

    use SCLs for Python 2.7 and change to just reporting done

--- install_goldstone.sh
+++ install_goldstone.sh
@@ -11,11 +11,7 @@ txtrst=$(tput sgr0) # reset
 
 function report_status() {
     d=`date`
-    if [ "$result" == "1" ]; then
-        echo -e "${d}	${stage}         ${green_text}[ PASS ]${txtrst}"
-    else
-        echo -e "${d} 	${stage}         ${red_text}[ FAIL ]${txtrst}"
-    fi
+    echo -e "${d}	${stage}         ${green_text}[ DONE ]${txtrst}"
 }
 
 function datestamp() {
@@ -25,14 +21,14 @@ function datestamp() {
 
 logrunname="install.log"
 
-stage="EPEL"; datestamp; result=$(setup_epel > $logrunname 2>&1 ); report_status
-stage="IPTABLES"; datestamp; result=$(config_iptables > $logrunname 2>&1); report_status
-stage="ELASTICSEARCH"; datestamp; result=$(install_elasticsearch > $logrunname 2>&1); report_status
-stage="LOGSTASH"; datestamp; result=$(install_logstash > $logrunname 2>&1); report_status
-stage="LOGGING"; datestamp; result=$(set_logging > $logrunname 2>&1); report_status
-stage="POSTGRESQL"; datestamp; result=$(install_pg > $logrunname 2>&1); report_status
-stage="GOLDSTONE"; datestamp; result=$(configure_goldstone > $logrunname 2>&1); report_status
-stage="CELERY"; datestamp; result=$(start_celery > $logrunname 2>&1 ); report_status
+stage="EPEL"; datestamp; result=$(setup_epel >> $logrunname 2>&1 ); report_status
+stage="IPTABLES"; datestamp; result=$(config_iptables >> $logrunname 2>&1); report_status
+stage="ELASTICSEARCH"; datestamp; result=$(install_elasticsearch >> $logrunname 2>&1); report_status
+stage="LOGSTASH"; datestamp; result=$(install_logstash >> $logrunname 2>&1); report_status
+stage="LOGGING"; datestamp; result=$(set_logging >> $logrunname 2>&1); report_status
+stage="POSTGRESQL"; datestamp; result=$(install_pg >> $logrunname 2>&1); report_status
+stage="GOLDSTONE"; datestamp; result=$(configure_goldstone >> $logrunname 2>&1); report_status
+stage="CELERY"; datestamp; result=$(start_celery >> $logrunname 2>&1 ); report_status
 
 
 # setup_epel
