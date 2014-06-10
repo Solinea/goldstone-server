88f7800a711e5b56f55efa2ba1935f47ac2c6fee
Ken Pepple <ken@solinea.com>
2014-06-10 14:08:21 -0700

    rebase problem

--- install_goldstone.sh
+++ install_goldstone.sh
@@ -3,23 +3,21 @@
 # ken@solinea.com
 # (c) Solinea, Inc  2014
 
-. ./install_functions.sh
-
+. ./install_functons.sh
 
 function report_status() {
     if [[ $result = 1 ]]; then
-        echo "${stage}         [ DONE ]"
-    else
-        echo "${stage}         [ FAIL ]"
+        print "${stage}         [ DONE ]"
+    elif
+        print "${stage}         [ FAIL ]"
     fi
 }
 
-logrunname="install.log"
 
-stage="EPEL"; result=$(setup_epel 2>&1 $logrunname); report_status
-stage="IPTABLES"; result=$(config_iptables 2>&1 $logrunname); report_status
-stage="ELASTICSEARCH"; result=$(install_elasticsearch 2>&1 $logrunname); report_status
-stage="LOGSTASH"; result=$(install_logstash 2>&1 $logrunname); report_status
-stage="POSTGRESQL"; result=$(install_pg 2>&1 $logrunname); report_status
-stage="GOLDSTONE"; result=$(configure_goldstone 2>&1 $logrunname); report_status
-stage="CELERY"; result=$(start_celery 2>&1 $logrunname); report_status
+stage="EPEL"; result=$(setup_epel); report_status()
+stage="IPTABLES"; result=$(config_iptables); report_status()
+stage="ELASTICSEARCH"; result=$(install_elasticsearch); report_status()
+stage="LOGSTASH"; result=$(install_logstash); report_status()
+stage="POSTGRESQL"; result=$(install_pg); report_status()
+stage="GOLDSTONE"; result=$(configure_goldstone); report_status()
+stage="CELERY"; result=$(start_celery); report_status()
