77e5c4c9b6326cc2e8f41d4b72096adeffc5e26f
Ken Pepple <ken@solinea.com>
2014-06-11 12:54:55 -0700

    use correct celerybeat binary for tasks; completely new startup scripts

--- install_goldstone.sh
+++ install_goldstone.sh
@@ -101,9 +101,9 @@ function start_celery() {
     cd /opt/goldstone
     python -c 'from goldstone.apps.core.tasks import create_daily_index; create_daily_index()'
     # export DJANGO_SETTINGS_MODULE=goldstone.settings.production; celery worker --app=goldstone --loglevel=warn --beat --uid=goldstone &
-    chkconfig --add celeryd
-    chkconfig celeryd on
-    service celeryd restart
+    chkconfig --add celerybeat
+    chkconfig celerybeat on
+    service celerybeat restart
 }
 
 function set_logging() {
