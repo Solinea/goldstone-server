12b540a4501093036a433ae50264c621c516bf56
John Stanford <john@solinea.com>
2014-07-04 12:25:22 -0500

    finished mocks for HostAvail tasks, views, and models

--- goldstone/apps/logging/tests.py
+++ goldstone/apps/logging/tests.py
@@ -29,84 +29,120 @@ logger = logging.getLogger(__name__)
 
 
 class TaskTests(SimpleTestCase):
-    """
-    Remember that celery should be running, and if changes have been made
-    to task code, it should be restarted.
-    """
 
-    def test_publish_host_stream_message(self):
-        """
-        Should be able to publish a message to redis and have the task receive
-        it.
-        """
-        host1_name = str(uuid.uuid4())
-        host1_time = datetime.now(tz=pytz.utc).isoformat()
-        task_id = str(uuid.uuid1())
-        body = {
-            "body": json.dumps({
-                'task': 'goldstone.apps.logging.tasks.process_host_stream',
-                'id': task_id,
-                'args': [host1_name, host1_time]
-            }),
-            "content-type": "application/json",
-            "properties": {
-                "delivery_info": {
-                    "priority": 0,
-                    "routing_key": "host_stream.#",
-                    "exchange": "default"
-                },
-                "delivery_mode": 2,
-                "delivery_tag": str(uuid.uuid4())
-            },
-            "content-encoding": "utf-8"
-        }
-        body = json.dumps(body)
-        r = redis.StrictRedis(host='localhost', port=6379, db=0)
-        r.lpush("host_stream", body)
+    @patch.object(HostAvailData, 'set')
+    def test_process_host_stream(self, set):
 
-        # this should wait for the task to complete (making it synchronous)
-        # task returns the key
-        key = 'host_stream.whitelist.' + host1_name
-        result = AsyncResult(task_id).get(timeout=6)
-        self.assertEqual(result, key)
+        # test when host exists in blacklist
+        set.return_value = None
+        result = process_host_stream('test123',
+                                     '2014-07-04T01:06:27.750046+00:00')
+        self.assertTrue(set.called)
+        self.assertEqual(result, set.return_value)
+        # test when host is not in blacklist
+        set.return_value = 'host_stream.whitelist.test123'
+        result = process_host_stream('test123',
+                                     '2014-07-04T01:06:27.750046+00:00')
+        self.assertEqual(set.call_count, 2)
+        self.assertEqual(result, set.return_value)
 
-        # the records should be stored in redis with keys of
-        # host_stream.whitelist.hostname and values of their respective
-        # timestamps
+    @patch.object(redis.StrictRedis, 'mget')
+    @patch.object(redis.StrictRedis, 'keys')
+    def test_check_host_avail(self, keys, mget):
+        keys.return_value = ['host_stream.whitelist.test123',
+                             'host_stream.whitelist.test456',
+                             'host_stream.whitelist.test789']
+        now = datetime.now(tz=pytz.utc)
+        last_year = now - timedelta(days=365)
 
-        host1_redis_time = r.get(key)
-        try:
-            self.assertEqual(host1_time, host1_redis_time)
-            r.delete(key)
-        except Exception:
-            r.delete(key)
-            raise
+        mget.return_value = [now.isoformat(),
+                             now.isoformat(),
+                             last_year.isoformat()]
 
-    def test_check_host_avail(self):
-        r = redis.StrictRedis(host='localhost', port=6379, db=0)
-        timestamp1 = (
-            datetime.now(tz=pytz.utc) -
-            settings.HOST_AVAILABLE_PING_THRESHOLD -
-            timedelta(hours=1)
-        ).strftime("%Y-%m-%dT%H:%M:%S.000Z")
-        timestamp2 = (
-            datetime.now(tz=pytz.utc) -
-            settings.HOST_AVAILABLE_PING_THRESHOLD +
-            timedelta(hours=1)
-        ).strftime("%Y-%m-%dT%H:%M:%S.000Z")
-        r.set('host_stream.whitelist.test', timestamp1)
-        r.set('host_stream.whitelist.test2', timestamp2)
-        sleep(1)
-        l = check_host_avail()
-        try:
-            self.assertIn('test', l)
-            self.assertNotIn('test2', l)
-            r.delete('host_stream.whitelist.test')
-            r.delete('host_stream.whitelist.test2')
-        except:
-            r.delete('host_stream.whitelist.test')
-            r.delete('host_stream.whitelist.test2')
-            raise
+        result = check_host_avail()
+        self.assertTrue(keys.called)
+        self.assertTrue(mget.called)
+        self.assertNotIn('test123', result)
+        self.assertNotIn('test456', result)
+        self.assertIn('test789', result)
+
+
+    # TODO this should be part of the integration test suite
+    # def test_publish_host_stream_message(self):
+    #     """
+    #     Should be able to publish a message to redis and have the task receive
+    #     it.
+    #     """
+    #     host1_name = str(uuid.uuid4())
+    #     host1_time = datetime.now(tz=pytz.utc).isoformat()
+    #     task_id = str(uuid.uuid1())
+    #     body = {
+    #         "body": json.dumps({
+    #             'task': 'goldstone.apps.logging.tasks.process_host_stream',
+    #             'id': task_id,
+    #             'args': [host1_name, host1_time]
+    #         }),
+    #         "content-type": "application/json",
+    #         "properties": {
+    #             "delivery_info": {
+    #                 "priority": 0,
+    #                 "routing_key": "host_stream.#",
+    #                 "exchange": "default"
+    #             },
+    #             "delivery_mode": 2,
+    #             "delivery_tag": str(uuid.uuid4())
+    #         },
+    #         "content-encoding": "utf-8"
+    #     }
+    #     body = json.dumps(body)
+    #     r = redis.StrictRedis(host='localhost', port=6379, db=0)
+    #     r.lpush("host_stream", body)
+    #
+    #     # this should wait for the task to complete (making it synchronous)
+    #     # task returns the key
+    #     key = 'host_stream.whitelist.' + host1_name
+    #     result = AsyncResult(task_id).get(timeout=6)
+    #     self.assertEqual(result, key)
+    #
+    #     # the records should be stored in redis with keys of
+    #     # host_stream.whitelist.hostname and values of their respective
+    #     # timestamps
+    #
+    #     host1_redis_time = r.get(key)
+    #     try:
+    #         self.assertEqual(host1_time, host1_redis_time)
+    #         r.delete(key)
+    #     except Exception:
+    #         r.delete(key)
+    #         raise
+
+
+    # TODO this should be part of the integration test suite
+    # def test_check_host_avail(self):
+    #     r = redis.StrictRedis(host='localhost', port=6379, db=0)
+    #     timestamp1 = (
+    #         datetime.now(tz=pytz.utc) -
+    #         settings.HOST_AVAILABLE_PING_THRESHOLD -
+    #         timedelta(hours=1)
+    #     ).strftime("%Y-%m-%dT%H:%M:%S.000Z")
+    #     timestamp2 = (
+    #         datetime.now(tz=pytz.utc) -
+    #         settings.HOST_AVAILABLE_PING_THRESHOLD +
+    #         timedelta(hours=1)
+    #     ).strftime("%Y-%m-%dT%H:%M:%S.000Z")
+    #     r.set('host_stream.whitelist.test', timestamp1)
+    #     r.set('host_stream.whitelist.test2', timestamp2)
+    #     sleep(1)
+    #     l = check_host_avail()
+    #     try:
+    #         self.assertIn('test', l)
+    #         self.assertNotIn('test2', l)
+    #         r.delete('host_stream.whitelist.test')
+    #         r.delete('host_stream.whitelist.test2')
+    #     except:
+    #         r.delete('host_stream.whitelist.test')
+    #         r.delete('host_stream.whitelist.test2')
+    #         raise
 
 
 class HostAvailModelTests(SimpleTestCase):
