71648d84e993f561200d3ae6475714145a31987f
John Stanford <john@solinea.com>
2014-07-04 19:16:16 -0500

    model and tests for to_whitelist, to_blacklist and delete

--- goldstone/apps/logging/models.py
+++ goldstone/apps/logging/models.py
@@ -47,18 +47,78 @@ class HostAvailData(RedisConnection):
             'blacklist': black_data
         }
 
-    def set(self, host, datetime_string):
+    def set(self, host, datetime_string, list_color='white'):
         """
         set or update the state of a host entry
         :param host: host name string
         :param datetime_string: string
+        :param list_color white|black
         :return: key or None
         """
 
-        if not self.conn.exists(self.black_prefix + host):
-            key = self.white_prefix + host
-            self.conn.set(key, datetime_string)
-            logger.debug("set key %s to %s", key, datetime_string)
-            return key
+        if list_color == 'white':
+            if not self.conn.exists(self.black_prefix + host):
+                key = self.white_prefix + host
+                self.conn.set(key, datetime_string)
+                logger.debug("set key %s to %s", key, datetime_string)
+                return key
+            else:
+                return None
         else:
-            return None
+            if not self.conn.exists(self.white_prefix + host):
+                key = self.black_prefix + host
+                self.conn.set(key, datetime_string)
+                logger.debug("set key %s to %s", key, datetime_string)
+                return key
+            else:
+                return None
+
+    def delete(self, host, list_color='both'):
+        """
+        delete a host from one or both of the white/black lists
+        :param host: host name string
+        :param white|black|both
+        :return: None
+        """
+        if list_color == 'white' or list_color == 'both':
+            self.conn.delete(self.white_prefix + host)
+        if list_color == 'black' or list_color == 'both':
+            self.conn.delete(self.black_prefix + host)
+
+    def to_blacklist(self, host):
+        """
+        move a host from the whitelist to the blacklist
+        :param host: host name string
+        :return: key or None
+        """
+        white_key = self.white_prefix + host
+        white_val = self.conn.get(white_key)
+        if white_val is not None:
+            self.delete(host, list_color='white')
+            return self.set(host, white_val, 'black')
+        else:
+            black_key = self.black_prefix + host
+            black_val = self.conn.get(black_key)
+            if black_val is not None:
+               return black_key
+            else:
+                return None
+
+    def to_whitelist(self, host):
+        """
+        move a host from the blacklist to the whitelist
+        :param host: host name string
+        :return: key or None
+        """
+        black_key = self.black_prefix + host
+        black_val = self.conn.get(black_key)
+        if black_val is not None:
+            self.delete(host, list_color='black')
+            return self.set(host, black_val, 'white')
+        else:
+            white_key = self.white_prefix + host
+            white_val = self.conn.get(white_key)
+            if white_val is not None:
+               return white_key
+            else:
+                return None
