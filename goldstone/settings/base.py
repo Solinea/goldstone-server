1f4ac7d7fcb13ba951ab04ac9742513d87f677bb
John Stanford <john@solinea.com>
2014-08-08 17:56:23 -0700

    added page_size parameter for rest calls.

--- goldstone/settings/base.py
+++ goldstone/settings/base.py
@@ -257,7 +257,10 @@ REST_FRAMEWORK = {
         'rest_framework.renderers.JSONRenderer',
         'rest_framework.renderers.BrowsableAPIRenderer',
     ),
-    'DEFAULT_FILTER_BACKENDS': ('rest_framework.filters.DjangoFilterBackend',)
+    'DEFAULT_FILTER_BACKENDS': ('rest_framework.filters.DjangoFilterBackend',),
+    'PAGINATE_BY': 10,                 # Default to 10
+    'PAGINATE_BY_PARAM': 'page_size',  # Allow client to override, using `?page_size=xxx`.
+    'MAX_PAGINATE_BY': 100             # Maximum limit allowed when using `?page_size=xxx`.
 }
 
 # controls the time examined for the log volume stats included in the
