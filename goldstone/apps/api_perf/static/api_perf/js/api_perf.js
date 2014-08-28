a5947730cbeab95ca79e4bea325f944301b42f35
Alex Jacobs <alex@solinea.com>
2014-08-28 16:02:55 -0700

    Basic POC - renders data obtained from $.get to chartspace and updates view based on changes to model.

--- goldstone/apps/api_perf/static/api_perf/js/api_perf.js
+++ goldstone/apps/api_perf/static/api_perf/js/api_perf.js
@@ -17,3 +17,57 @@
  */
 
 goldstone.namespace('apiPerf.report');
+
+// backbone test start
+
+    var ChartBase = Backbone.Model.extend({});
+
+    var ChartBaseView = Backbone.View.extend({
+
+        defaults: {
+            margin: {top: 20, right: 20, bottom: 30, left: 40},
+            width: 525,
+            height: 200
+        },
+
+        initialize: function(){
+
+            this.model.on('change:data', this.render, this);
+            var height = this.defaults.height;
+
+            var svg = d3.select("#api-perf-report-r3-c2").append("svg")
+                .attr("width", this.defaults.width + this.defaults.margin.left + this.defaults.margin.right)
+                .attr("height", this.defaults.height + this.defaults.margin.top + this.defaults.margin.bottom)
+                .append("g")
+                .attr("transform", "translate(" + this.defaults.margin.left + "," + this.defaults.margin.top + ")");
+
+            svg.selectAll("rect")
+                .data(this.model.attributes.data)
+                .enter().append("rect")
+                .attr("width", function(d) { return 4; })
+                .attr("height", function(d) { return 4; })
+                .attr("x", function(d, i) { return i*3; })
+                .attr("y", function(d, i) { return (height - d.max/10); });
+        },
+
+        render: function(){
+
+            var height = this.defaults.height;
+
+            console.log('model changed');
+
+            var svg = d3.select("#api-perf-report-r3-c2");
+            svg.selectAll("rect")
+            .data(this.model.attributes.data)
+            .attr("width", function(d) { return 4; })
+            .attr("height", function(d) { return 4; })
+            .attr("x", function(d, i) { return i*3; })
+            .attr("y", function(d, i) { return (height - d.max/10); })
+            .exit().remove();
+        }
+
+    });
+
+
+
+// backbone test end
