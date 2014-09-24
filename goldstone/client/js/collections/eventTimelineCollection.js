f592898225bd4fa39bed1bc9477775899a8fe5a7
Alex Jacobs <alex@solinea.com>
2014-09-23 18:09:29 -0700

    Initial rendering of Event Timeline chart. Saving progress.

---
+++ goldstone/client/js/collections/eventTimelineCollection.js
@@ -0,0 +1,15 @@
+// define collection and link to model
+
+var EventTimelineCollection = Backbone.Collection.extend({
+
+    parse: function(data) {
+        return JSON.parse(data);
+    },
+
+    model: EventTimelineModel,
+
+    initialize: function(options) {
+        this.url = options.url;
+        this.fetch();
+    }
+});
