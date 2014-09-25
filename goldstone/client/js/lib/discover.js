cd28ad5c83d226aaf29432f0b4781f9e4a932f5a
Alex Jacobs <alex@solinea.com>
2014-09-24 18:39:08 -0700

    Duplicates functionality of Node Availability chart, but with Backbone modularity.

---
+++ goldstone/client/js/lib/discover.js
@@ -0,0 +1,42 @@
+/**
+ * Copyright 2014 Solinea, Inc.
+ *
+ * Licensed under the Solinea Software License Agreement (goldstone),
+ * Version 1.0 (the "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at:
+ *
+ *     http://www.solinea.com/goldstone/LICENSE.pdf
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Author: John Stanford
+ */
+
+var renderEventTimelineCharts = function() {
+
+    //----------------------------
+    // instantiate charts via
+    // backbone collection / views
+
+
+    //---------------------------
+    // instantiate event timeline chart
+
+    var eventTimelineChart = new EventTimelineCollection({
+        //TODO: GET NEW DATA ENDPOINT
+        // url: "/cinder/api_perf?start=1408924756&end=1411516756&interval=32400s&render=false"
+        url: "/logging/nodes"
+    });
+
+    var eventTimelineChartView = new EventTimelineView({
+        collection: eventTimelineChart,
+        location: '#goldstone-discover-r1-c1',
+        width: $('#goldstone-discover-r1-c1').width(),
+        h: {"main": 450, "swim": 50}
+    });
+
+};
