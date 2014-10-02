1f05611a89f6ac4e82b4ebe4e0d72a332fa0abe9
Alex Jacobs <alex@solinea.com>
2014-10-02 16:57:04 -0700

    Refactors factory js files into lib file, and moves goldstone js files into top level js directory. Updates templates and test frmework.

--- test/include.conf.js
+++ test/include.conf.js
@@ -3,19 +3,7 @@
 
 module.exports = [
 
-    'goldstone/client/js/lib/base.js',
-    'goldstone/client/js/lib/goldstone.js',
-    'goldstone/client/js/lib/intelligence.js',
-    'goldstone/client/js/lib/search.js',
-
-    'goldstone/client/js/lib/nova.js',
-    'goldstone/client/js/lib/neutron.js',
-    'goldstone/client/js/lib/keystone.js',
-    'goldstone/client/js/lib/glance.js',
-    'goldstone/client/js/lib/cinder.js',
-    'goldstone/client/js/lib/api_perf.js',
-    'goldstone/client/js/lib/discover.js',
-
+    'goldstone/client/js/*.js',
     'goldstone/client/js/models/*.js',
     'goldstone/client/js/collections/*.js',
     'goldstone/client/js/views/*.js'
