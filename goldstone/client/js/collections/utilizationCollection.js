d057fcb5c6870a7b25acc8c80e291047e5128c53
Alex Jacobs <alex@solinea.com>
2014-10-08 15:31:10 -0700

    Implements Stacked Area Chart viz with dummy data generator.

--- goldstone/client/js/collections/utilizationCollection.js
+++ goldstone/client/js/collections/utilizationCollection.js
@@ -8,36 +8,68 @@ var UtilizationCollection = Backbone.Collection.extend({
 
     initialize: function(options) {
         this.url = options.url;
+        this.dummy = _.clone(this.dummy);
+        this.dummyGen();
         this.fetch();
     },
 
+    dummyGen: function() {
+        this.dummy = {
+            results: []
+        };
+
+
+        var day = 1412812619263;
+
+        for (var i = 0; i < Math.floor(Math.random() * 20) + 2; i++) {
+
+            var user = Math.floor(Math.random()*3300) / 100;
+            var system = Math.floor(Math.random()*3300) / 100;
+            var idle = Math.floor(Math.random()*3300) / 100;
+
+
+            var result = {
+                "date": day,
+                "User": user,
+                "System": system,
+                "Idle": idle,
+                "Wait": (100 - user - system - idle)
+            };
+
+            this.dummy.results.push(result);
+            day += 3600000;
+
+        }
+    },
+
+
     dummy: {
-        "name": "os.cpu.system",
-        "units": "percent",
         results: [{
-            "timestamp": 1000000000,
-            "value": 25
-        }, {
-            "timestamp": 1000360000,
-            "value": 20
-        }, {
-            "timestamp": 1000720000,
-            "value": 23
-        }, {
-            "timestamp": 1001080000,
-            "value": 35
-        }, {
-            "timestamp": 1001440000,
-            "value": 30
-        }, {
-            "timestamp": 1001800000,
-            "value": 15
-        }, {
-            "timestamp": 1002160000,
-            "value": 15
-        }, {
-            "timestamp": 1002540000,
-            "value": 20
-        }]
+                "date": "11-Oct-13",
+                "User": 41.62,
+                "System": 22.36,
+                "Idle": 25.58,
+                "Wait": 9.13,
+            }, {
+                "date": "12-Oct-13",
+                "User": 41.95,
+                "System": 22.15,
+                "Idle": 25.78,
+                "Wait": 8.79,
+            }, {
+                "date": "13-Oct-13",
+                "User": 37.64,
+                "System": 24.77,
+                "Idle": 25.96,
+                "Wait": 10.16,
+            }, {
+                "date": "14-Oct-13",
+                "User": 37.27,
+                "System": 24.65,
+                "Idle": 25.98,
+                "Wait": 10.59,
+            },
+
+        ]
     }
 });
