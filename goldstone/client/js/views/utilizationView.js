635cac508cc09a2d6c0e2405ee0e98c301777b4c
Alex Jacobs <alex@solinea.com>
2014-10-08 15:31:10 -0700

    Implements Stacked Area Chart viz with dummy data generator.

--- goldstone/client/js/views/utilizationView.js
+++ goldstone/client/js/views/utilizationView.js
@@ -2,10 +2,10 @@ var UtilizationView = Backbone.View.extend({
 
     defaults: {
         margin: {
-            left: 20,
-            bottom: 20,
             top: 20,
-            right: 20
+            right: 40,
+            bottom: 25,
+            left: 33
         }
     },
 
@@ -21,46 +21,55 @@ var UtilizationView = Backbone.View.extend({
 
         this.collection.on('sync', this.update, this);
 
-        ns.w = ns.width;
+        ns.mw = ns.width - ns.margin.left - ns.margin.right;
+        ns.mh = ns.width - ns.margin.top - ns.margin.bottom;
 
-        // initialize axis and svg
-        ns.bottomAxis = d3.svg.axis()
+        ns.parseDate = d3.time.format("%y-%b-%d").parse;
+        ns.formatPercent = d3.format(".0%");
+
+        ns.x = d3.time.scale()
+            .range([0, ns.mw]);
+
+        ns.y = d3.scale.linear()
+            .range([ns.mh, 0]);
+
+        ns.color = d3.scale.category20();
+
+        ns.xAxis = d3.svg.axis()
+            .scale(ns.x)
             .orient("bottom")
-            .ticks(5)
-            .tickFormat(d3.time.format("%H:%M"));
+            .ticks(5);
 
-        var color = d3.scale.category20();
-
-        ns.leftAxis = d3.svg.axis()
+        ns.yAxis = d3.svg.axis()
+            .scale(ns.y)
             .orient("left")
-            .ticks(5)
-            .tickFormat(d3.format(",.0f"));
+            .tickFormat(ns.formatPercent);
 
-        ns.xScale = d3.time.scale()
-            .range([ns.margin.left, ns.w-ns.margin.right]);
+        ns.area = d3.svg.area()
+            .x(function(d) {
+                return ns.x(d.date);
+            })
+            .y0(function(d) {
+                return ns.y(d.y0);
+            })
+            .y1(function(d) {
+                return ns.y(d.y0 + d.y);
+            });
 
-        ns.yScale = d3.time.scale()
-            .range([ns.w, ns.w - ns.margin.top - ns.margin.bottom]);
+        ns.stack = d3.layout.stack()
+            .values(function(d) {
+                return d.values;
+            });
 
         ns.svg = d3.select(ns.location).append("svg")
-            .attr("width", ns.w)
-            .attr("height", ns.w);
+            .attr("width", ns.mw + ns.margin.left + ns.margin.right)
+            .attr("height", ns.mh + ns.margin.top + ns.margin.bottom)
+            .append("g")
+            .attr("transform", "translate(" + ns.margin.left + "," + ns.margin.top + ")");
 
-
-        ns.graph = ns.svg.append("g").attr("id", "graph")
-            .attr("transform", "translate(0,0)");
-
-        ns.graph.append("g")
-            .attr("class", "xLower axis")
-            .attr("transform", "translate(0," + (ns.w - ns.margin.bottom) + ")");
-
-        ns.graph.append("g")
-            .attr("class", "yLeft axis")
-            .attr("transform", "translate("+ns.margin.left+",0)");
 
 
         $('<img id="spinner" src="' + blueSpinnerGif + '">').load(function() {
-            console.log(ns.width);
             $(this).appendTo(ns.location).css({
                 'position': 'relative',
                 'margin-left': (ns.width / 2),
@@ -78,65 +87,76 @@ var UtilizationView = Backbone.View.extend({
 
         var allTheLogs = this.collection.toJSON();
 
-        console.log(allTheLogs);
-
-        $(this.defaults.location).append('<br>');
-
-        var xEnd = moment(d3.min(_.map(allTheLogs, function(evt) {
-            return evt.timestamp;
-        })));
-
-        var xStart = moment(d3.max(_.map(allTheLogs, function(evt) {
-            return evt.timestamp;
-        })));
-
-        var yEnd = d3.min(_.map(allTheLogs, function(evt) {
-            return evt.value;
-        }));
-
-        var yStart = d3.max(_.map(allTheLogs, function(evt) {
-            return evt.value;
-        }));
-
-        ns.xScale = ns.xScale.domain([xEnd, xStart]);
-
-        ns.yScale = ns.xScale.domain([yStart, 0]);
-
-        // If we didn't receive any valid files, abort and pause
         if (allTheLogs.length === 0) {
             console.log('no data returned');
             return;
         }
 
-        // Shape the dataset
+        var data = allTheLogs;
 
-        ns.dataset = allTheLogs
-            .map(function(d) {
-                d.timestamp = moment(d.timestamp)._d;
-                return d;
+        ns.color.domain(d3.keys(data[0]).filter(function(key) {
+            return key !== "date";
+        }));
+
+        // data.forEach(function(d) {
+        //     d.date = ns.parseDate(d.date);
+        // });
+
+        var browsers = ns.stack(ns.color.domain().map(function(name) {
+            return {
+                name: name,
+                values: data.map(function(d) {
+                    return {
+                        date: d.date,
+                        y: d[name] / 100
+                    };
+                })
+            };
+        }));
+
+        ns.x.domain(d3.extent(data, function(d) {
+            return d.date;
+        }));
+
+        var browser = ns.svg.selectAll(".browser")
+            .data(browsers)
+            .enter().append("g")
+            .attr("class", "browser");
+
+        browser.append("path")
+            .attr("class", "area")
+            .attr("d", function(d) {
+                return ns.area(d.values);
+            })
+            .style("fill", function(d) {
+                return ns.color(d.name);
             });
 
+        browser.append("text")
+            .datum(function(d) {
+                return {
+                    name: d.name,
+                    value: d.values[d.values.length - 1]
+                };
+            })
+            .attr("transform", function(d) {
+                return "translate(" + ns.x(d.value.date) + "," + ns.y(d.value.y0 + d.value.y / 2) + ")";
+            })
+            .attr("x", 1)
+            .style("font-size", ".8em")
+            .text(function(d) {
+                return d.name;
+            });
 
-        // calculate the new domain.
-        // adjust each axis to its new scale.
+        ns.svg.append("g")
+            .attr("class", "x axis")
+            .attr("transform", "translate(0," + ns.mh + ")")
+            .call(ns.xAxis);
 
-        ns.bottomAxis.scale(ns.xScale);
-        ns.leftAxis.scale(ns.yScale);
+        ns.svg.append("g")
+            .attr("class", "y axis")
+            .call(ns.yAxis);
 
-        ns.svg.select(".xLower.axis")
-            .transition()
-            .call(ns.bottomAxis);
-
-        ns.svg.select(".yLeft.axis")
-            .transition()
-            .call(ns.leftAxis);
-
-
-
-
-        // _.each(allTheLogs, function(item) {
-        //     $(this.defaults.location).append(new Date((_.keys(item.timestamp) * 1000)) + '<br> Value: ' + item.value + '<br>');
-        // }, this);
     }
 
 });
