0381c2bb4648173253732117507d468552c24410
Alex Jacobs <alex@solinea.com>
2014-10-13 14:34:34 -0700

    Adds selectors for user/system/wait to perVmCpu viz.

--- goldstone/client/js/views/hypervisorVmCpuView.js
+++ goldstone/client/js/views/hypervisorVmCpuView.js
@@ -20,7 +20,7 @@ var HypervisorVmCpuView = Backbone.View.extend({
 
     defaults: {
         margin: {
-            top: 10,
+            top: 25,
             right: 10,
             bottom: 18,
             left: 25
@@ -40,7 +40,7 @@ var HypervisorVmCpuView = Backbone.View.extend({
         this.collection.on('sync', this.update, this);
 
         ns.mw = ns.width - ns.margin.left - ns.margin.right;
-        ns.mh = ns.width * 2 - ns.margin.top - ns.margin.bottom;
+        ns.mh = ns.width - ns.margin.top - ns.margin.bottom;
 
         ns.x = d3.time.scale()
             .range([0, ns.mw]);
@@ -50,7 +50,8 @@ var HypervisorVmCpuView = Backbone.View.extend({
 
         ns.xAxis = d3.svg.axis()
             .scale(ns.x)
-            .orient("bottom");
+            .orient("bottom")
+            .ticks(5);
 
         ns.yAxis = d3.svg.axis()
             .scale(ns.y)
@@ -61,7 +62,7 @@ var HypervisorVmCpuView = Backbone.View.extend({
                 return ns.x(d.date);
             })
             .y(function(d) {
-                return ns.y(d.close);
+                return ns.y(d[ns.selectedButton]);
             });
 
         ns.svg = d3.select(ns.location).append("svg")
@@ -70,6 +71,7 @@ var HypervisorVmCpuView = Backbone.View.extend({
             .append("g")
             .attr("transform", "translate(" + ns.margin.left + "," + ns.margin.top + ")");
 
+        this.appendButtons();
 
         // required in case spinner loading takes
         // longer than chart loading
@@ -104,14 +106,15 @@ var HypervisorVmCpuView = Backbone.View.extend({
             return;
         }
 
-        var data = allTheLogs;
+        ns.dataset = allTheLogs;
 
-        ns.x.domain(d3.extent(data, function(d){
+        ns.x.domain(d3.extent(ns.dataset, function(d) {
             return d.date;
         }));
-        ns.y.domain([0, d3.max(data, function(d){
-                    return d.close;
-                })]);
+
+        ns.y.domain([0, d3.max(ns.dataset, function(d) {
+            return d[ns.selectedButton];
+        })]);
 
         ns.svg.append("g")
             .attr("class", "x axis")
@@ -122,19 +125,80 @@ var HypervisorVmCpuView = Backbone.View.extend({
             .attr("class", "y axis")
             .call(ns.yAxis)
             .append("text")
-            // .attr("transform", "rotate(-90)")
-            .attr("y", 6)
+            .attr("y", -10)
             .attr("x", 3)
             .attr("dy", ".71em")
             .style("text-anchor", "left")
-            .text("figure out label");
+            .text("percent utilization(%)");
 
         ns.svg.append("path")
-            .datum(data)
+            .datum(ns.dataset)
             .attr("class", "line")
+            .attr("id", "dataPath")
             .attr("fill", "none")
             .attr("stroke", "#000")
             .attr("d", ns.line);
+
+    },
+
+    refresh: function() {
+        var ns = this.defaults;
+        var self = this;
+
+        if(ns.dataset === undefined){
+            return;
+        }
+
+        ns.y.domain([0, d3.max(ns.dataset, function(d) {
+            return d[ns.selectedButton];
+        })]);
+
+        ns.x.domain(d3.extent(ns.dataset, function(d) {
+            return d.date;
+        }));
+
+        d3.select(ns.location).selectAll("path")
+            .transition()
+            .duration(500)
+            .attr("d", ns.line);
+
+        d3.select(ns.location).select(".y.axis")
+            .transition()
+            .duration(500)
+            .call(ns.yAxis);
+
+        d3.select(ns.location).select(".x.axis")
+            .transition()
+            .duration(500)
+            .call(ns.xAxis);
+
+    },
+
+    appendButtons: function() {
+
+        var ns = this.defaults;
+        var self = this;
+
+        $(ns.location).find("#data-filterer")
+            .append("<div class='btn-group pull-left'>" +
+                "<div class='btn-group'>" +
+                "<button type='button' class='btn btn-default btn-sm active'>User</button>" +
+                "<button type='button' class='btn btn-default btn-sm'>System</button>" +
+                "<button type='button' class='btn btn-default btn-sm'>Wait</button>" +
+                "</div></div>"
+        );
+
+        $(function() {
+            $(ns.location).find("button").click(function() {
+                $("button.active").toggleClass("active");
+                $(this).toggleClass("active");
+                var buttonPressed = ($(this).context.innerText);
+                self.defaults.selectedButton = buttonPressed.toLowerCase();
+                self.refresh();
+            });
+        });
+
+        ns.selectedButton = 'user';
 
     }
 
