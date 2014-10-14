a0ae8142de3d3a18157f8aa8460f5400ca0396ab
Alex Jacobs <alex@solinea.com>
2014-10-14 16:18:48 -0700

    Renders multi-line output based on number of VMs.

--- goldstone/client/js/collections/hypervisorVmCpuCollection.js
+++ goldstone/client/js/collections/hypervisorVmCpuCollection.js
@@ -19,7 +19,6 @@
 var HypervisorVmCpuCollection = Backbone.Collection.extend({
 
     parse: function(data) {
-        console.log(this.dummy.results);
         return this.dummy.results;
     },
 
@@ -28,7 +27,7 @@ var HypervisorVmCpuCollection = Backbone.Collection.extend({
     initialize: function(options) {
         this.url = options.url;
         this.dummy = _.clone(this.dummy);
-        // this.dummyGen();
+        this.dummyGen();
         this.fetch();
     },
 
@@ -40,89 +39,108 @@ var HypervisorVmCpuCollection = Backbone.Collection.extend({
 
         var day = 1412812619263;
 
+        var randomizer = function() {
+            var result = Math.floor(Math.random() * 5000) /
+                100;
+            return result;
+        };
+
         for (var i = 0; i < Math.floor(Math.random() * 20) + 10; i++) {
 
-            var user = Math.floor(Math.random() * 5000) /
-                100;
-            var system = Math.floor(Math.random() * 5000) / 100;
-
             var result = {
-                "date": day,
-                "user": user,
-                "system": system,
-                "wait": (100 - user - system)
+                date: day,
+
+                user: [{
+                    vm1: randomizer(),
+                    vm2: randomizer(),
+                    vm3: randomizer(),
+                    vm4: randomizer(),
+                    vm5: randomizer()
+                }],
+                system: [{
+                    vm1: randomizer(),
+                    vm2: randomizer(),
+                    vm3: randomizer(),
+                    vm4: randomizer(),
+                    vm5: randomizer()
+                }],
+                wait: [{
+                    vm1: randomizer(),
+                    vm2: randomizer(),
+                    vm3: randomizer(),
+                    vm4: randomizer(),
+                    vm5: randomizer()
+                }]
+
             };
 
             this.dummy.results.push(result);
-            day += 3600000;
+            day += 360000;
 
         }
-        console.log(this.dummy.results);
-
     },
 
     dummy: {
-        results: [{
-            date: 1412812619263,
-            vmList: [{
-                vmName: "vm1",
-                user: 3.7,
-                system: 6.7,
-                wait: 75
-            }, {
-                vmName: "vm2",
-                user: 63.7,
-                system: 26.7,
-                wait: 25
-            }, {
-                vmName: "vm3",
-                user: 3.7,
-                system: 76.7,
-                wait: 45
-            }, {
-                vmName: "vm4",
-                user: 3.7,
-                system: 16.7,
-                wait: 85
-            }, {
-                vmName: "vm5",
-                user: 3.7,
-                system: 66.7,
-                wait: 65
-            }]
-        },
-        {
-            date: 1412817619263,
-            vmList: [{
-                vmName: "vm1",
-                user: 73.7,
-                system: 36.7,
-                wait: 25
-            }, {
-                vmName: "vm2",
-                user: 53.7,
-                system: 96.7,
-                wait: 70
-            }, {
-                vmName: "vm3",
-                user: 33.7,
-                system: 86.7,
-                wait: 35
-            }, {
-                vmName: "vm4",
-                user: 77.7,
-                system: 46.7,
-                wait: 35
-            }, {
-                vmName: "vm5",
-                user: 37.7,
-                system: 36.7,
-                wait: 75
-            }]
-        }
 
+        results: [
 
+            {
+                date: 1412812619263,
 
+                user: [{
+                    vm1: 50,
+                    vm2: 19,
+                    vm3: 11
+                }],
+                system: [{
+                    vm1: 10,
+                    vm2: 79,
+                    vm3: 31
+                }],
+                wait: [{
+                    vm1: 80,
+                    vm2: 39,
+                    vm3: 61
+                }]
+
+            }, {
+                date: 1412912619263,
+
+                user: [{
+                    vm1: 80,
+                    vm2: 29,
+                    vm3: 51
+                }],
+                system: [{
+                    vm1: 80,
+                    vm2: 59,
+                    vm3: 21
+                }],
+                wait: [{
+                    vm1: 70,
+                    vm2: 49,
+                    vm3: 71
+                }]
+
+            }, {
+                date: 1413012619263,
+
+                user: [{
+                    vm1: 60,
+                    vm2: 29,
+                    vm3: 51
+                }],
+                system: [{
+                    vm1: 80,
+                    vm2: 39,
+                    vm3: 81
+                }],
+                wait: [{
+                    vm1: 30,
+                    vm2: 79,
+                    vm3: 51
+                }]
+            }
         ]
     }
 
