46e9b4c11d6e61d80e71003f5faacb0aaea80192
Alex Jacobs <alex@solinea.com>
2014-10-25 12:31:40 -0700

    Updates integration tests for ApiPerfReport.

--- test/integration/integrationTests.js
+++ test/integration/integrationTests.js
@@ -4,12 +4,18 @@
 describe('apiPerfView.js spec', function() {
     beforeEach(function() {
 
+        // to answer GET requests
+        this.server = sinon.fakeServer.create();
+        this.server.respondWith("GET", "/something/fancy", [200, {
+            "Content-Type": "application/json"
+        }, '[]']);
+
         // confirm that dom is clear of view elements before each test:
         expect($('svg').length).to.equal(0);
         expect($('#spinner').length).to.equal(0);
 
         this.testCollection = new ApiPerfCollection({
-            url: '/nova/api_perf?start=1408554857&end=1411146857&interval=32400s&render=false'
+            url: '/something/fancy'
         });
         blueSpinnerGif = "goldstone/static/images/ajax-loader-solinea-blue.gif";
 
@@ -33,7 +39,25 @@ describe('apiPerfView.js spec', function() {
     });
     afterEach(function() {
         $('body').html('');
+        this.server.restore();
     });
+    describe('collection is constructed', function() {
+        it('should exist', function() {
+            var dataTest = JSON.stringify('hello');
+            assert.isDefined(this.testCollection, 'this.testCollection has been defined');
+            expect(this.testCollection.parse).to.be.a('function');
+            this.testCollection.initialize({
+                url: 'hi'
+            });
+            expect(this.testCollection.length).to.equal(1);
+            this.testCollection.add({
+                test1: 'test1'
+            });
+            expect(this.testCollection.length).to.equal(2);
+            this.testCollection.parse(dataTest);
+        });
+    });
+
     describe('view is constructed', function() {
         it('should exist', function() {
             assert.isDefined(this.testView, 'this.testView has been defined');
@@ -54,81 +78,18 @@ describe('apiPerfView.js spec', function() {
             expect($('svg').text()).to.not.include('Response was empty');
         });
         it('can handle a null server payload and append appropriate response', function() {
-            console.log($('svg').text());
-            expect($('svg').text()).to.not.include('Response was empty');
-            console.log(this.testCollection.toJSON());
+            this.update_spy = sinon.spy(this.testView, "update");
+            expect($('#noDataReturned').text()).to.equal('');
             this.testCollection.reset();
-            console.log(this.testCollection.toJSON());
             this.testView.update();
-            expect($('svg').text()).to.include('Response was empty');
-            console.log($('svg').text());
-            $('body').html('');
-            this.testCollection.add({url: '/nova/api_perf?start=1408554857&end=1411146857&interval=32400s&render=false'});
-            console.log(this.testCollection.toJSON());
+            expect($('#noDataReturned').text()).to.equal('No Data Returned');
+            this.testCollection.add({
+                url: '/blah'
+            });
             this.testView.update();
-            console.log($('svg').text());
-            expect($('svg').text()).to.not.include('Response was empty');
+            expect($('#noDataReturned').text()).to.equal('');
+            expect(this.update_spy.callCount).to.equal(2);
+            this.update_spy.restore();
         });
     });
-});
-
-
-
-
-describe("Backbone Object functionality", function() {
-    beforeEach(function() {
-
-        server = sinon.fakeServer.create();
-
-        JSONresponse = 'chartData: [{"2xx":20.0,"3xx":0.0,"4xx":0.0,"5xx":0.0,"avg":317.4,"count":20.0,"key":1409140800000.0,"max":559.0,"min":270.0,"std_deviation":61.1027004313,"sum":6348.0,"sum_of_squares":2089526.0,"variance":3733.54},{"2xx":938.0,"3xx":0.0,"4xx":0.0,"5xx":0.0,"avg":337.2590618337,"count":938.0,"key":1409173200000.0,"max":2981.0,"min":248.0,"std_deviation":130.4775305509,"sum":316349.0,"sum_of_squares":122660441.0,"variance":17024.3859786508},{"2xx":192.0,"3xx":0.0,"4xx":0.0,"5xx":0.0,"avg":313.7135416667,"count":192.0,"key":1409205600000.0,"max":754.0,"min":261.0,"std_deviation":47.228238198,"sum":60233.0,"sum_of_squares":19324165.0,"variance":2230.5064832899}';
-
-        server.respondWith('GET', '/*', [
-            200, {
-                'Content-Type': 'application/json'
-            },
-            JSON.stringify(JSONresponse)
-        ]);
-
-        this.nsReport_stub = {
-            start: new Date(1408554857000),
-            end: new Date(1411146857000),
-            interval: "32400s"
-        };
-
-
-        this.testApiChart = new ApiPerfCollection({
-            url: goldstone.nova.apiPerf.url(this.nsReport_stub.start, this.nsReport_stub.end, this.nsReport_stub.interval, false)
-        });
-
-        this.fetch_stub = sinon.stub(this.testApiChart, "fetch");
-
-        this.initialize_stub = sinon.stub(this.testApiChart, "initialize");
-
-        // this.render_stub = sinon.stub(this.testApiChart, "render");
-
-    });
-
-    afterEach(function() {
-        // this.render_stub.restore();
-        this.fetch_stub.restore();
-        this.initialize_stub.restore();
-        server.restore();
-
-    });
-
-    describe("Something about Model", function() {
-        it("something should happen", function() {
-
-            expect(this.nsReport_stub.start).to.be.a('date');
-            expect(this.nsReport_stub.end).to.be.a('date');
-            expect(this.nsReport_stub.interval).to.be.a('string');
-
-            this.testApiChart.fetch();
-            // server.respond();
-
-            // expect(this.fetch_stub).should.have.been.calledOnce();
-            sinon.assert.calledOnce(this.fetch_stub);
-        });
-    });
-
 });
