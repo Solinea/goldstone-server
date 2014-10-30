164b26013f2a2b87fd6d24b6b5e09914994489e4
Alex Jacobs <alex@solinea.com>
2014-10-30 13:20:50 -0700

    Updates integration tests for nodeReport 'service/report/events' tabs.

---
+++ test/integration/eventsReportView_integrationTests.js
@@ -0,0 +1,53 @@
+/*global sinon, todo, chai, describe, it, calledOnce*/
+//integration tests - serviceStatusView.js
+
+describe('eventsReportView.js spec', function() {
+    beforeEach(function() {
+        $('body').html('<div class="testContainer"></div>');
+
+        // to answer GET requests
+        this.server = sinon.fakeServer.create();
+        this.server.respondWith("GET", "/*", [200, {
+            "Content-Type": "application/json"
+        }, '{absolutely: "nothing"}']);
+
+        // confirm that dom is clear of view elements before each test:
+        expect($('svg').length).to.equal(0);
+        expect($('#spinner').length).to.equal(0);
+
+        blueSpinnerGif = "goldstone/static/images/ajax-loader-solinea-blue.gif";
+
+        this.testView = new EventsReportView({
+            el: '.testContainer',
+            width: 800
+        });
+    });
+    afterEach(function() {
+        $('body').html('');
+        this.server.restore();
+    });
+    describe('view is constructed', function() {
+        it('should exist', function() {
+            assert.isDefined(this.testView, 'this.testView has been defined');
+            expect(this.testView).to.be.an('object');
+            expect(this.testView.el).to.equal('.testContainer');
+            expect($(this.testView.el).text()).to.equal('No Events Data');
+        });
+        it('responds appropritely based on localStorage variables', function() {
+            this.update_spy = sinon.spy(this.testView, "update");
+            expect($('#availableReportResul').length).to.equal(0);
+            expect($('#noDataReturned').text()).to.equal('');
+            this.testView.update();
+
+            var testLocalStorageData = JSON.stringify({a:1, b:2, c:3});
+
+            localStorage.setItem('reportNodeData', testLocalStorageData);
+            $(this.testView.el).empty();
+            this.testView.update();
+            expect($(this.testView.el).text()).to.equal('Sample Data Load:');
+            expect(this.update_spy.callCount).to.equal(2);
+            this.update_spy.restore();
+        });
+    });
+
+});
