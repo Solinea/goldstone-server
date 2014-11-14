1ecd586f381421c9ecccb355af8d79ab3c2a440d
John Stanford <john@solinea.com>
2014-11-14 13:36:26 -0800

    metrics, events, and resports now have full query/filter/sort support

--- goldstone/apps/core/views.py
+++ goldstone/apps/core/views.py
@@ -167,66 +167,25 @@ class EventViewSet(ElasticViewSet):
     ordering = '-created'
 
 
-class OldEventViewSet(ModelViewSet):
-    model = EventType
-    serializer_class = EventSerializer
-    lookup_field = "_id"
-
-
-    # def get_object(self):
-    #     q = self.queryset
-    #     lookup_url_kwarg = self.lookup_url_kwarg or self.lookup_field
-    #     lookup = self.kwargs.get(lookup_url_kwarg, None)
-    #     if lookup is not None:
-    #         filter_kwargs = {self.lookup_field: lookup}
-    #     q_result = q.filter(**filter_kwargs)[:1].execute()
-    #     if q_result.count == 1:
-    #         obj = q_result.objects[0].get_object()
-    #         return obj
-    #     else:
-    #         raise Http404
-
-
-class MetricViewSet(ModelViewSet):
-    queryset = MetricType().search().query().order_by('-timestamp')
+# TODO should this be a read only viewset?
+class MetricViewSet(ElasticViewSet):
+    model = MetricType
     serializer_class = MetricSerializer
-    lookup_field = "_id"
+    lookup_field = '_id'
+    lookup_url_kwarg = '_id'
+    ordering = '-timestamp'
 
-    def list(self, request, *args, **kwargs):
-        # adding support filter params
-        params = request.QUERY_PARAMS.dict()
-        if params is not None:
-            # don't use the page related params as filters
-            if settings.REST_FRAMEWORK['PAGINATE_BY_PARAM'] in params:
-                del params[settings.REST_FRAMEWORK['PAGINATE_BY_PARAM']]
-            if 'page' in params:
-                del params['page']
-
-            self.queryset = MetricType().search().query().filter(**params). \
-                order_by('-timestamp')
-
-            return super(MetricViewSet, self).list(request, *args, **kwargs)
+    def retrieve(self, request, *args, **kwargs):
+        return HttpResponseNotAllowed('')
 
 
-class ReportViewSet(ReadOnlyModelViewSet):
-    queryset = ReportType().search().query().order_by('-timestamp')
+# TODO should this be a read only viewset?
+class ReportViewSet(ElasticViewSet):
+    model = ReportType
     serializer_class = ReportSerializer
     lookup_field = "_id"
-
-    def list(self, request, *args, **kwargs):
-        # adding support filter params
-        params = request.QUERY_PARAMS.dict()
-        if params != {}:
-            # don't use the page related params as filters
-            if settings.REST_FRAMEWORK['PAGINATE_BY_PARAM'] in params:
-                del params[settings.REST_FRAMEWORK['PAGINATE_BY_PARAM']]
-            if 'page' in params:
-                del params['page']
-
-            self.queryset = ReportType().search().query().filter(**params). \
-                order_by('-timestamp')
-
-        return super(ReportViewSet, self).list(request, *args, **kwargs)
+    lookup_url_kwarg = '_id'
+    ordering = '-timestamp'
 
     def retrieve(self, request, *args, **kwargs):
         return HttpResponseNotAllowed('')
