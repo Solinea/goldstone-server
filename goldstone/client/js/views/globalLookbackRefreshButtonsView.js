6eb5fe98d39a8e81932f092c6c8429880e56b6a9
Alex Jacobs <alex@solinea.com>
2014-11-17 15:37:53 -0800

    Creates backbone view for lookback/refresh buttons to be instantiated on relevant pages.

---
+++ goldstone/client/js/views/globalLookbackRefreshButtonsView.js
@@ -0,0 +1,77 @@
+/**
+ * Copyright 2014 Solinea, Inc.
+ *
+ * Licensed under the Solinea Software License Agreement (goldstone),
+ * Version 1.0 (the "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at:
+ *
+ *     http://www.solinea.com/goldstone/LICENSE.pdf
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Author: Alex Jacobs
+ */
+
+var GlobalLookbackRefreshButtonsView = Backbone.View.extend({
+
+    defaults: {},
+
+    initialize: function(options) {
+        this.options = options || {};
+        this.defaults = _.clone(this.defaults);
+        this.el = options.el;
+
+        var ns = this.defaults;
+        var self = this;
+
+        this.render();
+
+    },
+
+    render: function() {
+        console.log('rendering');
+        this.$el.html(this.template());
+        return this;
+    },
+
+    template: _.template('' +
+        '<div style="width:10%;" class="col-xl-1 pull-right">&nbsp;' +
+        '</div>' +
+        '<div class="col-xl-2 pull-right">' +
+        '<form class="global-refresh-selector" role="form">' +
+        '<div class="form-group">' +
+        '<div class="col-xl-1">' +
+        '<div class="input-group">' +
+        '<select class="form-control" id="global-refresh-range">' +
+        '<option value="15">refresh 15s</option>' +
+        '<option value="30" selected>refresh 30s</option>' +
+        '<option value="60">refresh 1m</option>' +
+        '<option value="300">refresh 5m</option>' +
+        '<option value="-1">refresh off</option>' +
+        '</select>' +
+        '</div>' +
+        '</div>' +
+        '</div>' +
+        '</form>' +
+        '</div>' +
+        '<div class="col-xl-1 pull-right">' +
+        '<form class="global-lookback-selector" role="form">' +
+        '<div class="form-group">' +
+        '<div class="col-xl-1">' +
+        '<div class="input-group">' +
+        '<select class="form-control" id="global-lookback-range">' +
+        '<option value="15">lookback 15m</option>' +
+        '<option value="60" selected>lookback 1h</option>' +
+        '<option value="360">lookback 6h</option>' +
+        '<option value="1440">lookback 1d</option>' +
+        '</select>' +
+        '</div>' +
+        '</div>' +
+        '</div>' +
+        '</form>' +
+        '</div>')
+});
