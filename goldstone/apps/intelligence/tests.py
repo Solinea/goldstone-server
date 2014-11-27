80b38fbb7e4f86e502bc8fb7fa089551bf35c0c9
John Stanford <john@solinea.com>
2014-11-26 18:35:40 -0800

    cleaned up the intelligence module tests

--- goldstone/apps/intelligence/tests.py
+++ goldstone/apps/intelligence/tests.py
@@ -34,40 +34,7 @@ logger = logging.getLogger(__name__)
 
 
 class LogDataModel(SimpleTestCase):
-    INDEX_NAME = 'logstash-test'
-    DOCUMENT_TYPE = 'logs'
     conn = Elasticsearch(settings.ES_SERVER)
-    template_f = gzip.open(os.path.join(os.path.dirname(__file__), "..",
-                                        "..", "..", "test_data",
-                                        "template.json.gz"), 'rb')
-    template = json.load(template_f)
-
-    try:
-        conn.indices.delete("_all")
-    finally:
-        {}
-
-    conn.indices.create(INDEX_NAME, body=template)
-
-    q = {"query": {"match_all": {}}}
-    data_f = gzip.open(os.path.join(os.path.dirname(__file__), "..", "..",
-                                    "..", "test_data",
-                                    "data.json.gz"))
-    data = json.load(data_f)
-    for dataset in data:
-        for event in dataset['hits']['hits']:
-            rv = conn.index(INDEX_NAME, event['_type'],
-                            event['_source'])
-
-    conn.indices.refresh([INDEX_NAME])
-    q = {"query": {"match_all": {}}}
-    rs = conn.search(body=q, index="_all")
-
-    def setUp(self):
-        pass
-
-    def tearDown(self):
-        pass
 
     def test_get_connection(self):
         q = dict(query={"match": {"type": "syslog"}})
