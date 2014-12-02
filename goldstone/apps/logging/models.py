7bfc6f2add7656bb3ef66d16c2f564d29f370d15
John Stanford <john@solinea.com>
2014-12-01 16:18:15 -0800

    WIP still framing out LoggingNode, need to push to lab

--- goldstone/apps/logging/models.py
+++ goldstone/apps/logging/models.py
@@ -12,19 +12,90 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 from datetime import datetime
+import json
+from arrow import arrow
+from django.conf import settings
 
 from django.db import models
 import logging
 from django.db.models import IntegerField
 from polymorphic import PolymorphicManager, PolymorphicQuerySet
 from polymorphic.query import Polymorphic_QuerySet_objects_per_request
+from pyes import BoolQuery, RangeQuery, ESRangeOp
 from goldstone.apps.core.models import Node, Event, NodeType
 from goldstone.apps.logging.es_models import LoggingNodeStats
+from goldstone.models import ESData
 
 
 __author__ = 'stanford'
 
 logger = logging.getLogger(__name__)
+
+
+class LoggingNodeStats(ESData):
+
+    _stats = None
+
+    def __init__(self, start_time=None, end_time=None):
+        """
+        Prime the statistics for all known nodes in the time range
+        :param start_time: arrow time
+        :param end_time: arrow time
+        :return:
+        """
+        self.end_time = arrow.utcnow if \
+            end_time is None else end_time
+
+        self.start_time = self.end_time.replace(
+            minutes=(-1 * settings.LOGGING_NODE_LOGSTATS_LOOKBACK_MINUTES)) \
+            if start_time is None else start_time
+
+        _query_value = BoolQuery(must=[
+            RangeQuery(qrange=ESRangeOp(
+                "@timestamp",
+                "gte", self.start_time.isoformat(),
+                "lte", self.end_time.isoformat())),
+        ]).serialize()
+
+        _aggs_value = {
+            "by_host": {
+                "terms": {
+                    "field": "host.raw"
+                },
+                "aggregations": {
+                    "by_level": {
+                        "terms": {
+                            "field": "loglevel",
+                            "min_doc_count": 0
+                        }
+                    }
+                }
+            }
+        }
+
+        query = {
+            "query": _query_value,
+            "aggs": _aggs_value
+        }
+
+        logger.debug("query = %s", json.dumps(query))
+
+        rs = self._conn.search(
+            index=self.get_index_names('logstash-'), doc_type="syslog",
+            body=query, size=0)
+
+        self._stats = rs["aggregations"]["by_host"]["buckets"]
+
+    def for_node(self, name):
+
+        nodes = [x for x in self._stats if x['key'] == name]
+        if len(nodes) > 0:
+            return {
+                bucket['key']: bucket['doc_count']
+                for bucket in nodes[0]['by_level']['buckets']
+            }
+        else:
+            return None
 
 
 class LoggingNodeType(NodeType):
@@ -82,10 +153,3 @@ class LoggingNode(Node):
 
     _mt = LoggingNodeType()
     es_objects = LoggingNodeType
-
-
-class LoggingEvent(Event):
-    """
-    Represents an event harvested from the log event stream.
-    """
-    pass
