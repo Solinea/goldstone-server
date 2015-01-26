96b648784a5cfdfeff55c02fb39205c8da8e1933
John Stanford <john@solinea.com>
2015-01-26 15:14:39 -0800

    pushing lab settings upstream now that we have fine grained control over what goes in the package. fixed syntax problem in local_oak config files.

--- goldstone/settings/local_oak_c1.py
+++ goldstone/settings/local_oak_c1.py
@@ -3,12 +3,12 @@ from .development import *
 OS_USERNAME = 'admin'
 OS_TENANT_NAME = 'admin'
 OS_PASSWORD = '2caa6a4d9c9d49ce'
-OS_AUTH_URL= 'http://10.10.20.10:5000/v2.0/'
+OS_AUTH_URL = 'http://10.10.10.23:5000/v2.0/'
 
 #
 # override ES settings
 #
-ES_HOST = "10.10.20.201"
+ES_HOST = "10.10.10.201"
 ES_PORT = "9200"
 ES_SERVER = ES_HOST + ":" + ES_PORT
 
