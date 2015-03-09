89d2d4102cc67e78bf958df35793392373170c09
John DeRosa <john.derosa@solinea.com>
2015-03-09 15:08:05 -0700

    GOLD-645: First draft of fab openstack_init.

--- goldstone/tenants/models.py
+++ goldstone/tenants/models.py
@@ -1,4 +1,4 @@
-"""Tenant models."""
+"""Tenant and tenant setting models."""
 # Copyright 2015 Solinea, Inc.
 #
 # Licensed under the Solinea Software License Agreement (goldstone),
@@ -26,8 +26,7 @@ class Tenant(models.Model):
     """
 
     name = models.CharField(max_length=settings.TENANT_NAME_MAX_LENGTH,
-                            unique=True,
-                            help_text="The tenant's name")
+                            unique=True)
     owner = models.CharField(max_length=settings.TENANT_OWNER_MAX_LENGTH,
                              help_text="The name of the tenant's owner")
     owner_contact = \
@@ -41,3 +40,36 @@ class Tenant(models.Model):
         """Return a useful string."""
 
         return u'%s owned by %s' % (self.name, self.owner)
+
+
+class Cloud(models.Model):
+    """An OpenStack cloud, a.k.a. OpenStack tenant, which is contained within a
+    Goldstone tenant."""
+
+    openstack_tenant_name = \
+        models.CharField(max_length=settings.OS_TENANT_NAME_MAX_LENGTH)
+
+    openstack_username = \
+        models.CharField(max_length=settings.OS_USERNAME_MAX_LENGTH)
+
+    openstack_password = \
+        models.CharField(max_length=settings.OS_PASSWORD_MAX_LENGTH)
+
+    openstack_auth_url = \
+        models.CharField(max_length=settings.OS_AUTH_URL_MAX_LENGTH)
+
+    tenant = models.ForeignKey(Tenant)
+
+    # This allows URLs to identify a row using a UUID value.
+    uuid = UUIDField(auto=True)
+
+    class Meta:             # pylint: disable=C1001,C0111,W0232
+        unique_together = ("openstack_tenant_name",
+                           "openstack_username",
+                           "tenant")
+
+    def __unicode__(self):
+        """Return a useful string."""
+
+        return u'%s, contained within %s' % (self.openstack_tenant_name,
+                                             self.tenant.name)
