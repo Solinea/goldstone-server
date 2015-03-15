a883326bda9b11e9d39e937f3f119e48a00b7e07
John Stanford <john@solinea.com>
2015-03-15 12:32:19 -0700

    added c2_gs2 settings file

---
+++ goldstone/settings/dev_oak_c2_gs2.py
@@ -0,0 +1,11 @@
+from .development import *       # pylint: disable=W0614,W0401
+
+#
+# override ES settings
+#
+ES_HOST = "10.10.20.202"
+ES_PORT = "9200"
+ES_SERVER = {'hosts': [ES_HOST + ":" + ES_PORT]}
+
+# ElasticUtils Settings
+ES_URLS = [ES_HOST + ":" + ES_PORT]
