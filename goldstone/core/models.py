7e344d877acbb1d32819028a95d5893b2c7af7d8
John DeRosa <john.derosa@solinea.com>
2015-03-28 15:15:12 -0700

    GOLD-706: pep8, pylint. _add_edges unit tests are done, and all pass.

--- goldstone/core/models.py
+++ goldstone/core/models.py
@@ -19,7 +19,7 @@ from django_extensions.db.fields import UUIDField, CreationDateTimeField, \
 from polymorphic import PolymorphicModel
 from goldstone.apps.drfes.models import DailyIndexDocType
 from goldstone.apps.logging.models import LogData, LogEvent
-from goldstone.utils import utc_now, get_keystone_client, get_nova_client
+from goldstone.utils import utc_now
 
 from elasticsearch_dsl.query import Q, QueryString
 import networkx
