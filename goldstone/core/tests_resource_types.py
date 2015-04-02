21a2911ca60c96c5282ea582be851d9705c0fce4
John DeRosa <john.derosa@solinea.com>
2015-04-02 16:24:20 -0700

    GOLD-716: Resource_types unit tests, up through Host from_node.

--- goldstone/core/tests_resource_types.py
+++ goldstone/core/tests_resource_types.py
@@ -16,13 +16,15 @@ from django.conf import settings
 from django.test import SimpleTestCase
 import elasticsearch
 from elasticsearch.client import IndicesClient
+from functools import partial
 import mock
 from mock import patch
 from rest_framework.test import APISimpleTestCase
 
 from .models import resources, Image, ServerGroup, NovaLimits, GraphNode, \
     PolyResource, Host, resource_types, Aggregate, Hypervisor, Port, \
-    Cloudpipe, Network, Project, Server, AvailabilityZone
+    Cloudpipe, Network, Project, Server, AvailabilityZone, Flavor, \
+    FlavorExtraSpec
 from . import tasks
 from .utils import custom_exception_handler, _add_edges, process_resource_type
 
@@ -38,8 +40,14 @@ TYPE = settings.R_ATTRIBUTE.TYPE
 MATCHING_FN = settings.R_ATTRIBUTE.MATCHING_FN
 
 
-def _test(type_from, data_from, identity_from, key_from,
-          type_to, data_to, identity_to, key_to):
+def _dictassign(thedict, key, value):
+    """A callable that does thedict[key] = value."""
+
+    thedict[key] = value
+
+
+def _test(type_from, data_from, identity_from, match_from_key_fn,
+          type_to, data_to, identity_to, match_to_key_fn):
     """Test two resource_types nodes.
 
     This function modifies data_from and to_from.
@@ -50,16 +58,18 @@ def _test(type_from, data_from, identity_from, key_from,
     :type data_from: dict
     :param identity_from: The identity() value against which to test
     :type identity_from: str
-    :param key_from: The key to modify for the matching_fn tests
-    :type key_from: str
+    :param match_from_key_fn: A one-argument function to modify the value used
+                              in the matching_fn test
+    :type match_from_key_fn: Callable
     :param type_to: The type of the "to" node in the resource_types graph
     :type type_to: PolyResource subclass
     :param data_to: Type_to's initial test data.
     :type data_to: dict
     :param identity_to: The identity() value against which to test
     :type identity_to: str
-    :param key_to: The key to modify for the matching_fn tests
-    :type key_to: str
+    :param match_to_key_fn: A one-argument function to modify the value used
+                            in the matching_fn test
+    :type match_to_key_fn: Callable
 
     """
 
@@ -71,32 +81,21 @@ def _test(type_from, data_from, identity_from, key_from,
     edges = resource_types.graph.out_edges(type_from, data=True)
     edge = [x for x in edges if x[1] == type_to][0][2]
 
+    # Test one being None
+    match_from_key_fn(None)
+    assert not edge[MATCHING_FN](data_from, data_to)
+
     # Test both being None
-    data_from[key_from] = None
-    data_to[key_to] = None
-    assert not edge[MATCHING_FN](data_from, data_to)
-
-    # Test one missing
-    data_from[key_from] = "42"
-    del data_to[key_to]
-    assert not edge[MATCHING_FN](data_from, data_to)
-
-    del data_from[key_from]
-    data_to[key_to] = "42"
-    assert not edge[MATCHING_FN](data_from, data_to)
-
-    # Test both missing
-    del data_to[key_to]
+    match_to_key_fn(None)
     assert not edge[MATCHING_FN](data_from, data_to)
 
     # Test no match
-    data_from[key_from] = "4445"
-    data_to[key_to] = "4444"
+    match_from_key_fn("4445")
+    match_to_key_fn("4444")
     assert not edge[MATCHING_FN](data_from, data_to)
 
     # Test match
-    data_from[key_from] = "4445"
-    data_to[key_to] = "4445"
+    match_to_key_fn("4445")
     assert edge[MATCHING_FN](data_from, data_to)
 
 
@@ -192,11 +191,11 @@ class ResourceTypesTests(SimpleTestCase):
         _test(Image,
               IMAGE,
               '0ae46ce1-80e5-447e-b0e8-9eeec81af920',
-              "id",
+              partial(_dictassign, IMAGE, "id"),
               Server,
               SERVER,
               'ee662ff5-3de6-46cb-8b85-4eb4317beb7c',
-              "id")
+              partial(_dictassign, SERVER, "id"))
 
     def test_availability_zone_aggregate(self):
         """Test the AvailabilityZone - Aggregate entry."""
@@ -224,7 +223,7 @@ class ResourceTypesTests(SimpleTestCase):
             u'zoneName': u'internal',
             u'zoneState': {u'available': True}
             }
-        
+
         AGGREGATE = {u'availability_zone': None,
                      u'created_at': u'2015-04-01T18:36:03.000000',
                      u'deleted': False,
@@ -238,11 +237,11 @@ class ResourceTypesTests(SimpleTestCase):
         _test(AvailabilityZone,
               AVAILABILITY_ZONE,
               "internal",
-              "zoneName",
+              partial(_dictassign, AVAILABILITY_ZONE, "zoneName"),
               Aggregate,
               AGGREGATE,
               'test-aggregate1',
-              "availability_zone")
+              partial(_dictassign, AGGREGATE, "availability_zone"))
 
     def test_availability_zone_host(self):
         """Test the AvailabilityZone - Host entry."""
@@ -276,8 +275,297 @@ class ResourceTypesTests(SimpleTestCase):
         _test(AvailabilityZone,
               AVAILABILITY_ZONE,
               "internal",
-              "zoneName",
+              partial(_dictassign, AVAILABILITY_ZONE, "zoneName"),
               Host,
               HOST,
               'ctrl-01',
-              "zone")
+              partial(_dictassign, HOST, "zone"))
+
+    def test_cloudpipe(self):
+        """Test the Cloudpipe entry."""
+
+        # Test data.
+        CLOUDPIPE = {}
+
+        SERVER = {u'OS-DCF:diskConfig': u'MANUAL',
+                  u'OS-EXT-AZ:availability_zone': u'nova',
+                  u'OS-EXT-SRV-ATTR:host': u'john.oak.solinea.com',
+                  u'OS-EXT-SRV-ATTR:hypervisor_hostname':
+                  u'john.oak.solinea.com',
+                  u'OS-EXT-SRV-ATTR:instance_name': u'instance-00000001',
+                  u'OS-EXT-STS:power_state': 4,
+                  u'OS-EXT-STS:task_state': None,
+                  u'OS-EXT-STS:vm_state': u'stopped',
+                  u'OS-SRV-USG:launched_at': u'2015-01-26T14:01:37.000000',
+                  u'OS-SRV-USG:terminated_at': None,
+                  u'accessIPv4': u'',
+                  u'accessIPv6': u'',
+                  u'addresses':
+                  {u'demo-net':
+                   [{u'OS-EXT-IPS-MAC:mac_addr': u'fa:00:00:7f:2a:00',
+                     u'OS-EXT-IPS:type': u'fixed',
+                     u'addr': u'192.168.1.1',
+                     u'version': 4},
+                    {u'OS-EXT-IPS-MAC:mac_addr': u'fa:00:00:7f:2a:00',
+                     u'OS-EXT-IPS:type': u'floating',
+                     u'addr': u'10.11.12.13',
+                     u'version': 4}]},
+                  u'config_drive': u'',
+                  u'created': u'2015-01-26T14:00:42Z',
+                  u'flavor': {u'id': u'1',
+                              u'links':
+                              [{u'href':
+                                u'http://10.11.12.13:8774/'
+                                u'7077765ed0df43b1b23d43c9c290daf9/flavors/1',
+                                u'rel': u'bookmark'}]},
+                  u'hostId':
+                  u'78f689fe281dbb1deb8e42ac188a9734faf430ddc905b556b74f6144',
+                  u'id': u'ee662ff5-3de6-46cb-8b85-4eb4317beb7c',
+                  u'image': {u'id': u'0ae46ce1-80e5-447e-b0e8-9eeec81af920',
+                             u'links':
+                             [{u'href':
+                               u'http://10.11.12.13:8774/'
+                               u'7077765ed0df43b1b23d43c9c290daf9/'
+                               u'images/0ae46ce1-80e5-447e-b0e8-9eeec81af920',
+                               u'rel': u'bookmark'}]},
+                  u'key_name': None,
+                  u'links':
+                  [{u'href':
+                    u'http://10.10.20.10:8774/v2/7077765ed0df43b1b23d'
+                    u'43c9c290daf9/servers/'
+                    u'ee662ff5-3de6-46cb-8b85-4eb4317beb7c',
+                    u'rel': u'self'},
+                   {u'href':
+                    u'http://10.10.20.10:8774/7077765ed0df43'
+                    u'b1b23d43c9c290daf9/servers/ee662ff5-3de6-46cb-'
+                    u'8b85-4eb4317beb7c',
+                    u'rel': u'bookmark'}],
+                  u'metadata': {},
+                  u'name': u'instance2',
+                  u'os-extended-volumes:volumes_attached': [],
+                  u'security_groups': [{u'name': u'default'}],
+                  u'status': u'SHUTOFF',
+                  u'tenant_id': u'56762288eea24ab08a3b6d06f5a37c14',
+                  u'updated': u'2015-03-04T01:27:22Z',
+                  u'user_id': u'2bb2f66f20cb47e9be48a91941e3353b'}
+
+        # _test(Cloudpipe,
+        #       CLOUDPIPE,
+        #       '0ae46ce1-80e5-447e-b0e8-9eeec81af920',
+        #       "id",
+        #       Server,
+        #       SERVER,
+        #       'ee662ff5-3de6-46cb-8b85-4eb4317beb7c',
+        #       "id")
+
+        # TODO: Write this test. Couldn't make a cloudpipe today, for some odd
+        # reason.
+        pass
+
+    def test_flavor_flavorextraspec(self):
+        """Test the Flavor - FlavorExtraSpec entry."""
+
+        # Test data.
+        FLAVOR = {u'OS-FLV-DISABLED:disabled': False,
+                  u'OS-FLV-EXT-DATA:ephemeral': 0,
+                  u'disk': 80,
+                  u'id': u'4',
+                  u'links': [{u'href':
+                              u'http://10.11.12.13:8774/v2/7077765ed0df43b1b'
+                              u'23d43c9c290daf9/flavors/4',
+                              u'rel': u'self'},
+                             {u'href': u'http://10.11.12.13:8774/7077765ed0'
+                              u'df43b1b23d43c9c290daf9/flavors/4',
+                              u'rel': u'bookmark'}],
+                  u'name': u'm1.large',
+                  u'os-flavor-access:is_public': True,
+                  u'ram': 8192,
+                  u'rxtx_factor': 1.0,
+                  u'swap': u'',
+                  u'vcpus': 4}
+
+        # TODO: Find out what this payload really looks like.
+        FLAVOR_EXTRA_SPEC = {u'name': u'm1.large',
+                             u'were': u'making this up',
+                             }
+
+        _test(Flavor,
+              FLAVOR,
+              "m1.large",
+              partial(_dictassign, FLAVOR, "name"),
+              FlavorExtraSpec,
+              FLAVOR_EXTRA_SPEC,
+              'm1.large',
+              partial(_dictassign, FLAVOR_EXTRA_SPEC, "name"))
+
+    def test_flavor_server(self):
+        """Test the Flavor - Server entry."""
+
+        # Test data.
+        FLAVOR = {u'OS-FLV-DISABLED:disabled': False,
+                  u'OS-FLV-EXT-DATA:ephemeral': 0,
+                  u'disk': 80,
+                  u'id': u'4',
+                  u'links': [{u'href':
+                              u'http://10.11.12.13:8774/v2/7077765ed0df43b1b'
+                              u'23d43c9c290daf9/flavors/4',
+                              u'rel': u'self'},
+                             {u'href': u'http://10.11.12.13:8774/7077765ed0'
+                              u'df43b1b23d43c9c290daf9/flavors/4',
+                              u'rel': u'bookmark'}],
+                  u'name': u'm1.large',
+                  u'os-flavor-access:is_public': True,
+                  u'ram': 8192,
+                  u'rxtx_factor': 1.0,
+                  u'swap': u'',
+                  u'vcpus': 4}
+
+        SERVER = {u'OS-DCF:diskConfig': u'MANUAL',
+                  u'OS-EXT-AZ:availability_zone': u'nova',
+                  u'OS-EXT-SRV-ATTR:host': u'john.oak.solinea.com',
+                  u'OS-EXT-SRV-ATTR:hypervisor_hostname':
+                  u'john.oak.solinea.com',
+                  u'OS-EXT-SRV-ATTR:instance_name': u'instance-00000001',
+                  u'OS-EXT-STS:power_state': 4,
+                  u'OS-EXT-STS:task_state': None,
+                  u'OS-EXT-STS:vm_state': u'stopped',
+                  u'OS-SRV-USG:launched_at': u'2015-01-26T14:01:37.000000',
+                  u'OS-SRV-USG:terminated_at': None,
+                  u'accessIPv4': u'',
+                  u'accessIPv6': u'',
+                  u'addresses':
+                  {u'demo-net':
+                   [{u'OS-EXT-IPS-MAC:mac_addr': u'fa:00:00:7f:2a:00',
+                     u'OS-EXT-IPS:type': u'fixed',
+                     u'addr': u'192.168.1.1',
+                     u'version': 4},
+                    {u'OS-EXT-IPS-MAC:mac_addr': u'fa:00:00:7f:2a:00',
+                     u'OS-EXT-IPS:type': u'floating',
+                     u'addr': u'10.11.12.13',
+                     u'version': 4}]},
+                  u'config_drive': u'',
+                  u'created': u'2015-01-26T14:00:42Z',
+                  u'flavor': {u'id': u'1',
+                              u'links':
+                              [{u'href':
+                                u'http://10.11.12.13:8774/'
+                                u'7077765ed0df43b1b23d43c9c290daf9/flavors/1',
+                                u'rel': u'bookmark'}]},
+                  u'hostId':
+                  u'78f689fe281dbb1deb8e42ac188a9734faf430ddc905b556b74f6144',
+                  u'id': u'ee662ff5-3de6-46cb-8b85-4eb4317beb7c',
+                  u'image': {u'id': u'0ae46ce1-80e5-447e-b0e8-9eeec81af920',
+                             u'links':
+                             [{u'href':
+                               u'http://10.11.12.13:8774/'
+                               u'7077765ed0df43b1b23d43c9c290daf9/'
+                               u'images/0ae46ce1-80e5-447e-b0e8-9eeec81af920',
+                               u'rel': u'bookmark'}]},
+                  u'key_name': None,
+                  u'links':
+                  [{u'href':
+                    u'http://10.10.20.10:8774/v2/7077765ed0df43b1b23d'
+                    u'43c9c290daf9/servers/'
+                    u'ee662ff5-3de6-46cb-8b85-4eb4317beb7c',
+                    u'rel': u'self'},
+                   {u'href':
+                    u'http://10.10.20.10:8774/7077765ed0df43'
+                    u'b1b23d43c9c290daf9/servers/ee662ff5-3de6-46cb-'
+                    u'8b85-4eb4317beb7c',
+                    u'rel': u'bookmark'}],
+                  u'metadata': {},
+                  u'name': u'instance2',
+                  u'os-extended-volumes:volumes_attached': [],
+                  u'security_groups': [{u'name': u'default'}],
+                  u'status': u'SHUTOFF',
+                  u'tenant_id': u'56762288eea24ab08a3b6d06f5a37c14',
+                  u'updated': u'2015-03-04T01:27:22Z',
+                  u'user_id': u'2bb2f66f20cb47e9be48a91941e3353b'}
+
+        def _serverassign(value):
+            """A callable that loads a server dict's identity with value."""
+
+            SERVER["flavor"]["id"] = value
+
+        _test(Flavor,
+              FLAVOR,
+              "m1.large",
+              partial(_dictassign, FLAVOR, "id"),
+              Server,
+              SERVER,
+              'ee662ff5-3de6-46cb-8b85-4eb4317beb7c',
+              _serverassign)
+
+    def test_host_aggregate(self):
+        """Test the Host - Aggregate entry."""
+
+        # Test data.
+        HOST = {u'host_name': u'ctrl-01', u'zone': u'internal'}
+
+        AGGREGATE = {u'availability_zone': None,
+                     u'created_at': u'2015-04-01T18:36:03.000000',
+                     u'deleted': False,
+                     u'deleted_at': None,
+                     u'hosts': [u'bob.solinea.com'],
+                     u'id': 1,
+                     u'metadata': {},
+                     u'name': u'test-aggregate1',
+                     u'updated_at': None}
+
+        def _aggregateassign(value):
+            """A callable that loads an aggregate dict's identity with
+            value."""
+
+            AGGREGATE["hosts"] = ["bob", "marley", value]
+
+        _test(Host,
+              HOST,
+              'ctrl-01',
+              partial(_dictassign, HOST, "host_name"),
+              Aggregate,
+              AGGREGATE,
+              'test-aggregate1',
+              _aggregateassign)
+
+    def test_host_hypervisor(self):
+        """Test the Host - Hypervisor entry."""
+
+        # Test data.
+        HOST = {u'host_name': u'ctrl-01', u'zone': u'internal'}
+
+        HYPERVISOR = {u'cpu_info':
+                      u'{"vendor": "Intel", "model": "Westmere", '
+                      u'"arch": "x86_64", '
+                      u'"features": ["tsc-deadline", "dtes64", "vmx", "erms", '
+                      u'"xtpr", "smep", "est", "monitor", "3dnowprefetch", '
+                      u'"tm", "pclmuldq", "acpi", "vme", "tm2", "ht", "pdcm", '
+                      u'"ds", "invtsc", "rdtscp", "ss", "pbe", "ds_cpl", '
+                      u'"movbe", "rdrand"], "topology": {"cores": 8, '
+                      u'"threads": 1, "sockets": 1}}',
+                      u'current_workload': 0,
+                      u'disk_available_least': 1,
+                      u'free_disk_gb': 7,
+                      u'free_ram_mb': 10304,
+                      u'host_ip': u'10.10.20.21',
+                      u'hypervisor_hostname': u'john.solinea.com',
+                      u'hypervisor_type': u'QEMU',
+                      u'hypervisor_version': 12001,
+                      u'id': 1,
+                      u'local_gb': 49,
+                      u'local_gb_used': 42,
+                      u'memory_mb': 15936,
+                      u'memory_mb_used': 5632,
+                      u'running_vms': 4,
+                      u'service': {u'host': u'rsrc-02.c2.oak.solinea.com',
+                                   u'id': 5},
+                      u'vcpus': 8,
+                      u'vcpus_used': 4}
+
+        _test(Host,
+              HOST,
+              "ctrl-01",
+              partial(_dictassign, HOST, "host_name"),
+              Hypervisor,
+              HYPERVISOR,
+              1,
+              partial(_dictassign, HYPERVISOR, "hypervisor_hostname"))
