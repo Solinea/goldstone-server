826ed514370aac18d74ae15fc40519b7e63899cd
Alex Jacobs <alex@solinea.com>
2015-04-14 17:28:02 -0700

    Converts cpuResource collection/view to work with new api format.

--- goldstone/client/js/collections/cpuResourceCollection.js
+++ goldstone/client/js/collections/cpuResourceCollection.js
@@ -33,7 +33,11 @@ var CpuResourceCollection = Backbone.Collection.extend({
     defaults: {},
 
     parse: function(data) {
-        return data;
+        if (data && data.results) {
+            return data.results;
+        } else {
+            return [];
+        }
     },
 
     model: GoldstoneBaseModel,
@@ -55,10 +59,12 @@ var CpuResourceCollection = Backbone.Collection.extend({
         // the number of minutes specified by the selector
 
         var ns = this.defaults;
-        ns.reportParams.end = +new Date();
+
         ns.reportParams.start = (+new Date()) - (ns.globalLookback * 1000 * 60);
-        ns.reportParams.interval = '' + Math.round(1 * ns.globalLookback) + "s";
-        this.url = ns.urlPrefix + '?start=' + Math.floor(ns.reportParams.start / 1000) + '&end=' + Math.floor(ns.reportParams.end / 1000) + '&interval=' + ns.reportParams.interval;
+        this.url = ns.urlPrefix + '?name__prefix=nova.hypervisor.vcpus&@timestamp__range={"gte":' +
+            moment(ns.reportParams.start).valueOf() + '}';
     }
 
+    // creates a url similar to:
+    // /core/metrics?name__prefix=nova.hypervisor.vcpus&@timestamp__range={"gte":1426887188000}
 });
