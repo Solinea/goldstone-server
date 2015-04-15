fbc36c4ec2b50aea0bb4be1ff91247dc4761828a
John DeRosa <john.derosa@solinea.com>
2015-04-15 09:18:10 -0700

    GOLD-714: Move goldstone/apps/cinder to goldstone/cinder.

--- goldstone/core/views.py
+++ goldstone/core/views.py
@@ -145,8 +145,7 @@ class NavTreeView(RetrieveAPIView, TopologyMixin):
             as KeystoneDiscoverTree
         from goldstone.apps.glance.utils import DiscoverTree \
             as GlanceDiscoverTree
-        from goldstone.apps.cinder.utils import DiscoverTree \
-            as CinderDiscoverTree
+        from goldstone.cinder.utils import DiscoverTree as CinderDiscoverTree
         from goldstone.apps.nova.utils import DiscoverTree \
             as NovaDiscoverTree
 
