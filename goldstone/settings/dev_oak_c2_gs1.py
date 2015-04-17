ccf3edb107d51cedb6412dc4a80c393f683ddd3b
John DeRosa <john.derosa@solinea.com>
2015-04-17 14:59:59 -0700

    GOLD-729: Delete internal settings files.

--- goldstone/settings/dev_oak_c2_gs1.py
+++
@@ -1,9 +0,0 @@
-"""Development settings, with Elasticsearch remote and PostgreSQL local."""
-from .development import *           # pylint: disable=W0614,W0401
-
-#
-# override ES settings
-#
-ES_HOST = "10.10.20.201"
-ES_PORT = "9200"
-ES_SERVER = {'hosts': [ES_HOST + ":" + ES_PORT]}
