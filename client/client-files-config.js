e3742df5b72fa8c1ba05dd1a20ab5c865057b93b
Alex Jacobs <alex@solinea.com>
2015-07-16 17:56:57 -0700

    Migrate /opentrail/ js docs into /addons/ as it looks like addons will be containing their own javascript.

--- client/client-files-config.js
+++ client/client-files-config.js
@@ -32,7 +32,7 @@ module.exports = {
         'client/js/preload/utilizationCpuView.js',
         'client/js/models/*.js',
         'client/js/collections/*.js',
-        'client/js/opentrail/*.js',
+        'client/js/addons/*.js',
         'client/js/views/*.js'
     ],
     lib: ['client/js/lib/jquery.js', 'client/js/lib/bootstrap.js', 'client/js/lib/jquery.dataTables.js', 'client/js/lib/dataTables.bootstrap.js', 'client/js/lib/colorbrewer.js', 'client/js/lib/d3.js', 'client/js/lib/d3-tip.js', 'client/js/lib/d3-legend.js', 'client/js/lib/underscore.js', 'client/js/lib/backbone.js', 'client/js/lib/moment-with-locales.js', 'client/js/lib/moment-timezone-with-data-2010-2020.js'],
