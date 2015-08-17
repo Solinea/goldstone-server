fa0c5d7a6e664cc7b05823343c11dc0d1fbf4ef2
John Stanford <john@solinea.com>
2015-08-17 12:18:23 -0700

    fixed file write perm issue in entrypoint

--- bin/docker_entrypoint.sh
+++ bin/docker_entrypoint.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 python manage.py syncdb --noinput --migrate  # Apply database migrations
 
-# uncomment when we have gunicorn figured out and settings set accordingly
-# python manage.py collectstatic --noinput     # Collect static files
-
-if [[ ! -f /.gs_tenant_initialized ]] ; then
-    fab -f installer_fabfile.py docker_install && touch /.gs_tenant_initialized
+if [[ ! -f /app/.gs_tenant_initialized ]] ; then
+    fab -f installer_fabfile.py docker_install && touch /app/.gs_tenant_initialized
 fi
 
 echo Starting Gunicorn.
