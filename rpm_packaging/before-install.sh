cb4f94633e266e378e05e77983db9494739d0b6c
Ken Pepple <ken@solinea.com>
2016-01-10 12:11:18 -0800

    syntax

--- rpm_packaging/before-install.sh
+++ rpm_packaging/before-install.sh
@@ -1,4 +1,3 @@
-/usr/bin/getent group goldstone \
-    || /usr/sbin/groupadd -r goldstone
+/usr/bin/getent group goldstone || /usr/sbin/groupadd -r goldstone
 /usr/bin/getent passwd goldstone \
     || /usr/sbin/useradd -r -g goldstone -d %{prefix}/goldstone -s /sbin/nologin goldstone
