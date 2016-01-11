001a4b0119376470484b71d7c3c4a9a79da575d6
Alex Jacobs <alex@solinea.com>
2016-01-11 15:30:52 -0800

    Reorganizes charts and removes collections/views/tests for deprecated elements.

--- client/js/views/topologyPageView.js
+++ client/js/views/topologyPageView.js
@@ -16,57 +16,12 @@
 
 var TopologyPageView = GoldstoneBasePageView.extend({
 
+    // overwritten as there is not trigger functionality to topology
     triggerChange: function(change) {
-        if (change === 'lookbackSelectorChanged') {
-            this.eventTimelineChartView.trigger('lookbackSelectorChanged');
-            this.nodeAvailChartView.trigger('lookbackSelectorChanged');
-        }
-
-        if (change === 'lookbackIntervalReached') {
-            this.eventTimelineChartView.trigger('lookbackIntervalReached');
-            this.nodeAvailChartView.trigger('lookbackIntervalReached');
-        }
+        return true;
     },
 
     renderCharts: function() {
-
-        //---------------------------
-        // instantiate event timeline chart
-
-        this.eventTimelineChart = new EventTimelineCollection({});
-
-        this.eventTimelineChartView = new EventTimelineView({
-            collection: this.eventTimelineChart,
-            el: '#goldstone-discover-r1-c1',
-            chartTitle: goldstone.translate('Event Timeline'),
-            height: 300,
-            h: {
-                "main": 100,
-                "padding": 30,
-                "tooltipPadding": 50
-            },
-            infoText: 'eventTimeline',
-            width: $('#goldstone-discover-r1-c1').width()
-        });
-
-        //---------------------------
-        // instantiate Node Availability chart
-
-        this.nodeAvailChart = new NodeAvailCollection({});
-
-        this.nodeAvailChartView = new NodeAvailView({
-            chartTitle: goldstone.translate('Node Availability'),
-            collection: this.nodeAvailChart,
-            el: '#goldstone-discover-r1-c2',
-            height: 300,
-            h: {
-                "main": 150,
-                "swim": 50
-            },
-            infoText: 'nodeAvailability',
-            width: $('#goldstone-discover-r1-c2').width()
-        });
-
 
         //---------------------------
         // instantiate Cloud Topology chart
@@ -79,28 +34,20 @@ var TopologyPageView = GoldstoneBasePageView.extend({
             blueSpinnerGif: blueSpinnerGif,
             collection: this.discoverTreeCollection,
             chartTitle: goldstone.translate('Cloud Topology'),
-            el: '#goldstone-discover-r2-c1',
+            el: '#goldstone-discover-r1-c1',
             height: 600,
             infoText: 'discoverCloudTopology',
-            multiRsrcViewEl: '#goldstone-discover-r2-c2',
-            width: $('#goldstone-discover-r2-c2').width(),
+            multiRsrcViewEl: '#goldstone-discover-r1-c2',
+            width: $('#goldstone-discover-r1-c2').width(),
         });
 
     },
 
     template: _.template('' +
-
         '<div id="goldstone-discover-r1" class="row">' +
         '<div id="goldstone-discover-r1-c1" class="col-md-6"></div>' +
         '<div id="goldstone-discover-r1-c2" class="col-md-6"></div>' +
-        '</div>' +
-
-        '<div id="goldstone-discover-r2" class="row">' +
-        '<div id="goldstone-discover-r2-c1" class="col-md-6"></div>' +
-        '<div id="goldstone-discover-r2-c2" class="col-md-6"></div>' +
-        '</div>' +
-
-        '<div class="row"><br><br></div>'
+        '</div>' 
     )
 
 });
