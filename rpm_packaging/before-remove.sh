b5f58f1ab122c4e3c1ef0f8983c9e1ec1b97ceda
Ken Pepple <ken@solinea.com>
2016-01-11 19:57:32 -0800

    check for old images and processes on removing package

--- rpm_packaging/before-remove.sh
+++ rpm_packaging/before-remove.sh
@@ -4,6 +4,10 @@ systemctl disable goldstone-server
 
 # remove stopped containers
 echo "Removing containers..."
-docker rm -f $(docker ps -a | grep goldstone | awk '{print $1}' | xargs) || /bin/true
+if [ `docker ps -a | grep goldstone | wc -l` -gt 0 ]; then
+  docker rm -f $(docker ps -a | grep goldstone | awk '{print $1}' | xargs) || /bin/true
+fi
 echo "Removing images..."
-docker rmi -f $(docker images | grep goldstone | awk '{print $3}' | xargs) || /bin/true
+if [ `docker images -a | grep goldstone | wc -l` -gt 0 ]; then
+  docker rmi -f $(docker images | grep goldstone | awk '{print $3}' | xargs) || /bin/true
+fi
