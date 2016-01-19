31c6dbeb6ffc3d0021ee3556988121f5533b3e17
Ken Pepple <ken@solinea.com>
2016-01-18 18:46:36 -0800

    upgrade docker-compose to 1.5.2

--- rpm_packaging/before-install.sh
+++ rpm_packaging/before-install.sh
@@ -4,7 +4,7 @@
 
 # install docker-compose
 export GS_PATH="/opt"
-export DC_URL="https://github.com/docker/compose/releases/download/1.5.0/docker-compose-"
+export DC_URL="https://github.com/docker/compose/releases/download/1.5.2/docker-compose-"
 
 if [[ $# == 1 && $1 == 1 ]] ; then
     echo "Installing docker-compose to %{prefix}/goldstone/bin"
