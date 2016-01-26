449c323a34a253e7b681ef1f2d1382b5c5f6d141
Alex Jacobs <alex@solinea.com>
2016-01-26 14:45:56 -0800

    Migrates predefinedSearchView out of compliance and into goldstone-server.

---
+++ client/js/views/predefinedSearchView.js
@@ -0,0 +1,123 @@
+/**
+ * Copyright 2015 Solinea, Inc.
+ *
+ * Licensed under the Solinea Software License Agreement (goldstone),
+ * Version 1.0 (the "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at:
+ *
+ *     http://www.solinea.com/goldstone/LICENSE.pdf
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/*
+compliance/defined_search/ results structure:
+
+{
+    "uuid": "4ed7499e-d0c6-4d0b-be67-0418cd4b5d60",
+    "name": "failed authorization",
+    "owner": "compliance",
+    "description": "Defined Search",
+    "query": "{ \"query\": { \"bool\": { \"must\": [ { \"match\": { \"component\": \"keystone\" } }, { \"match_phrase\": { \"openstack_message\": \"authorization failed\" } } ] } } }",
+    "protected": true,
+    "index_prefix": "logstash-*",
+    "doc_type": "syslog",
+    "timestamp_field": "@timestamp",
+    "last_start": null,
+    "last_end": null,
+    "target_interval": 0,
+    "created": null,
+    "updated": null
+}
+
+*/
+
+PredefinedSearchView = GoldstoneBaseView.extend({
+
+    instanceSpecificInit: function() {
+        this.processOptions();
+        this.getPredefinedSearches();
+    },
+
+    getPredefinedSearches: function() {
+        var self = this;
+
+        $.get('/core/saved_search/?page_size=1000&index_prefix=logstash-*').
+        done(
+            function(result) {
+                if (result.results) {
+                    self.predefinedSearches = result.results;
+                    self.render();
+                } else {
+                    console.log('unknown result format');
+                }
+            }).
+        fail(function(result) {
+            console.log('failed defined search ', result);
+        });
+    },
+
+    populatePredefinedSearches: function(arr) {
+        var result = '';
+
+        _.each(arr, function(item) {
+            result += '<li data-uuid=' + item.uuid + '>' + goldstone.translate(item.name) + '</li>';
+        });
+
+        return result;
+    },
+
+    processListeners: function() {
+        var self = this;
+        $('.compliance-predefined-search-container .dropdown-menu').on('click', 'li', function(item) {
+            var clickedUuid = $(this).data('uuid');
+            var constructedUrlForTable = '/compliance/defined_search/' + clickedUuid + '/results/';
+
+            self.collection.urlBase = '/compliance/defined_search/' + clickedUuid + '/results/';
+            self.collection.urlGenerator();
+            var constructedUrlforViz = self.collection.url;
+            self.fetchResults(constructedUrlforViz, constructedUrlForTable);
+        });
+    },
+
+    fetchResults: function(vizUrl, tableUrl) {
+        var self = this;
+        $.get(vizUrl)
+            .done(function(res) {
+                self.trigger('clickedUuidViz', [res, vizUrl]);
+            })
+            .fail(function(err) {
+                console.error(err);
+            });
+
+        $.get(tableUrl)
+            .done(function(res) {
+                self.trigger('clickedUuidTable', [res, tableUrl]);
+            })
+            .fail(function(err) {
+                console.error(err);
+            });
+    },
+
+    template: _.template('' +
+        '<li role="presentation" class="dropdown">' +
+        '<a class = "droptown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">' +
+        '<%= goldstone.translate("Predefined Searches") %> <span class="caret"></span>' +
+        '</a>' +
+        '<ul class="dropdown-menu">' +
+        '<%= this.populatePredefinedSearches(this.predefinedSearches) %>' +
+        '</ul>' +
+        '</li>'
+    ),
+
+    render: function() {
+        $(this.el).html(this.template());
+        this.processListeners();
+        return this;
+    }
+
+});
--- goldstone/static/bundle/bundle.js
+++ goldstone/static/bundle/bundle.js
@@ -7690,47 +7690,34 @@ var LogSearchPageView = GoldstoneBasePageView.extend({
             this.setZoomed(trueFalse);
         });
 
+        // render predefinedSearch Dropdown
+        this.predefinedSearchModule = new PredefinedSearchView({
+            className: 'compliance-predefined-search nav nav-pills',
+            tagName: 'ul',
+            collection: new GoldstoneBaseCollection({
+                skipFetch: true,
+                urlBase: '',
+                addRange: function() {
+                    return '?@timestamp__range={"gte":' + this.gte + ',"lte":' + this.epochNow + '}';
+                },
+                addInterval: function(interval) {
+                    return '&interval=' + interval + 's';
+                },
+            })
+        });
 
-        this.viewsToStopListening = [this.logBrowserVizCollection, this.logBrowserViz, this.logBrowserTableCollection, this.logBrowserTable];
+        $('.compliance-predefined-search-container').html(this.predefinedSearchModule.el);
 
-
-        // check for compliance addon and render predefined search bar if present
-        if (goldstone.returnAddonPresent('compliance')) {
-            if (goldstone.compliance.PredefinedSearchView) {
-                this.predefinedSearchModule = new goldstone.compliance.PredefinedSearchView({
-                    className: 'compliance-predefined-search nav nav-pills',
-                    tagName: 'ul',
-                    collection: new GoldstoneBaseCollection({
-                        skipFetch: true,
-                        urlBase: '',
-                        addRange: function() {
-                            return '?@timestamp__range={"gte":' + this.gte + ',"lte":' + this.epochNow + '}';
-                        },
-                        addInterval: function(interval) {
-                            return '&interval=' + interval + 's';
-                        },
-                    })
-                });
-
-                $('.compliance-predefined-search-container').html(this.predefinedSearchModule.el);
-            }
-
-            // stopListening to predefinedSearchModule upon close, if present
-            if (this.predefinedSearchModule !== undefined) {
-                this.viewsToStopListening.push(this.predefinedSearchModule);
-            }
-
-            // subscribe logBrowserViz to click events on predefined
-            // search dropdown to fetch results.
-            this.listenTo(this.predefinedSearchModule, 'clickedUuidViz', function(uuid) {
-                // self.logBrowserTable.predefinedSearch(uuid[1]);
-                self.logBrowserViz.predefinedSearch(uuid[0]);
-            });
-            this.listenTo(this.predefinedSearchModule, 'clickedUuidTable', function(uuid) {
-                self.logBrowserTable.predefinedSearch(uuid[1]);
-                // self.logBrowserViz.predefinedSearch(uuid[0]);
-            });
-        }
+        // subscribe logBrowserViz to click events on predefined
+        // search dropdown to fetch results.
+        this.listenTo(this.predefinedSearchModule, 'clickedUuidViz', function(uuid) {
+            // self.logBrowserTable.predefinedSearch(uuid[1]);
+            self.logBrowserViz.predefinedSearch(uuid[0]);
+        });
+        this.listenTo(this.predefinedSearchModule, 'clickedUuidTable', function(uuid) {
+            self.logBrowserTable.predefinedSearch(uuid[1]);
+            // self.logBrowserViz.predefinedSearch(uuid[0]);
+        });
 
         // set up a chain of events between viz and table to uddate
         // table when updating viz.
@@ -7738,6 +7725,9 @@ var LogSearchPageView = GoldstoneBasePageView.extend({
             self.logBrowserTableCollection.filter = self.logBrowserViz.filter;
             self.logBrowserTable.update();
         });
+
+        // destroy listeners and views upon page close
+        this.viewsToStopListening = [this.logBrowserVizCollection, this.logBrowserViz, this.logBrowserTableCollection, this.logBrowserTable, this.predefinedSearchModule];
 
     },
 
@@ -10167,6 +10157,130 @@ var PasswordResetView = GoldstoneBaseView.extend({
 
     render: function() {
         this.$el.html(this.template());
+        return this;
+    }
+
+});
+;
+/**
+ * Copyright 2015 Solinea, Inc.
+ *
+ * Licensed under the Solinea Software License Agreement (goldstone),
+ * Version 1.0 (the "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at:
+ *
+ *     http://www.solinea.com/goldstone/LICENSE.pdf
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/*
+compliance/defined_search/ results structure:
+
+{
+    "uuid": "4ed7499e-d0c6-4d0b-be67-0418cd4b5d60",
+    "name": "failed authorization",
+    "owner": "compliance",
+    "description": "Defined Search",
+    "query": "{ \"query\": { \"bool\": { \"must\": [ { \"match\": { \"component\": \"keystone\" } }, { \"match_phrase\": { \"openstack_message\": \"authorization failed\" } } ] } } }",
+    "protected": true,
+    "index_prefix": "logstash-*",
+    "doc_type": "syslog",
+    "timestamp_field": "@timestamp",
+    "last_start": null,
+    "last_end": null,
+    "target_interval": 0,
+    "created": null,
+    "updated": null
+}
+
+*/
+
+PredefinedSearchView = GoldstoneBaseView.extend({
+
+    instanceSpecificInit: function() {
+        this.processOptions();
+        this.getPredefinedSearches();
+    },
+
+    getPredefinedSearches: function() {
+        var self = this;
+
+        $.get('/core/saved_search/?page_size=1000&index_prefix=logstash-*').
+        done(
+            function(result) {
+                if (result.results) {
+                    self.predefinedSearches = result.results;
+                    self.render();
+                } else {
+                    console.log('unknown result format');
+                }
+            }).
+        fail(function(result) {
+            console.log('failed defined search ', result);
+        });
+    },
+
+    populatePredefinedSearches: function(arr) {
+        var result = '';
+
+        _.each(arr, function(item) {
+            result += '<li data-uuid=' + item.uuid + '>' + goldstone.translate(item.name) + '</li>';
+        });
+
+        return result;
+    },
+
+    processListeners: function() {
+        var self = this;
+        $('.compliance-predefined-search-container .dropdown-menu').on('click', 'li', function(item) {
+            var clickedUuid = $(this).data('uuid');
+            var constructedUrlForTable = '/compliance/defined_search/' + clickedUuid + '/results/';
+
+            self.collection.urlBase = '/compliance/defined_search/' + clickedUuid + '/results/';
+            self.collection.urlGenerator();
+            var constructedUrlforViz = self.collection.url;
+            self.fetchResults(constructedUrlforViz, constructedUrlForTable);
+        });
+    },
+
+    fetchResults: function(vizUrl, tableUrl) {
+        var self = this;
+        $.get(vizUrl)
+            .done(function(res) {
+                self.trigger('clickedUuidViz', [res, vizUrl]);
+            })
+            .fail(function(err) {
+                console.error(err);
+            });
+
+        $.get(tableUrl)
+            .done(function(res) {
+                self.trigger('clickedUuidTable', [res, tableUrl]);
+            })
+            .fail(function(err) {
+                console.error(err);
+            });
+    },
+
+    template: _.template('' +
+        '<li role="presentation" class="dropdown">' +
+        '<a class = "droptown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">' +
+        '<%= goldstone.translate("Predefined Searches") %> <span class="caret"></span>' +
+        '</a>' +
+        '<ul class="dropdown-menu">' +
+        '<%= this.populatePredefinedSearches(this.predefinedSearches) %>' +
+        '</ul>' +
+        '</li>'
+    ),
+
+    render: function() {
+        $(this.el).html(this.template());
+        this.processListeners();
         return this;
     }
 
