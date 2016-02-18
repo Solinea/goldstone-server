3290ee855b3fbd57739ab5b520ab7fccd719879a
Alex Jacobs <alex@solinea.com>
2016-02-17 16:55:59 -0800

    Adds passwordResetView and newPasswordView back to repo.

---
+++ client/js/views/passwordResetView.js
@@ -0,0 +1,61 @@
+/**
+ * Copyright 2015 Solinea, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+var PasswordResetView = GoldstoneBaseView.extend({
+
+    initialize: function(options) {
+        this.addHandlers();
+    },
+
+    addHandlers: function() {
+        var self = this;
+
+        $('.login-form').on('submit', function(e) {
+            e.preventDefault();
+            self.submitRequest($(this).serialize());
+        });
+    },
+
+    submitRequest: function(input) {
+        var self = this;
+
+        // Upon clicking the submit button, the serialized user input is sent
+        // via $.post to check the credentials. If successful, invoke "done"
+        // if not, invoke "fail"
+
+        $.post('/accounts/password/reset/', input, function() {})
+            .done(function(success) {
+
+                // and add a message to the top of the screen that logs what
+                // is returned from the call
+                goldstone.raiseInfo('Password reset instructions have been emailed to you<br>Please click the link in your email');
+            })
+            .fail(function(fail) {
+                // and add a message to the top of the screen that logs what
+                // is returned from the call
+
+                // TODO: change this after SMTP handling is set up
+                // to reflect the proper error
+                goldstone.raiseInfo(fail.responseJSON.detail);
+            });
+    },
+
+    render: function() {
+        this.$el.html(this.template());
+        return this;
+    }
+
+});
--- goldstone/static/bundle/bundle.js
+++ goldstone/static/bundle/bundle.js
@@ -9025,6 +9025,99 @@ var MultiRscsView = GoldstoneBaseView.extend({
  * limitations under the License.
  */
 
+var NewPasswordView = GoldstoneBaseView.extend({
+
+    initialize: function(options) {
+        this.getUidToken();
+        this.addHandlers();
+    },
+
+    getUidToken: function() {
+        this.uidToken = window.location.search.slice(1);
+    },
+
+    addHandlers: function() {
+        var self = this;
+
+        $('.login-form').on('submit', function(e) {
+            e.preventDefault();
+
+            var $password = $('#password');
+            var $confirm_password = $('#confirm_password');
+
+            if ($password.val() !== $confirm_password.val()) {
+                goldstone.raiseWarning("Passwords don't match.");
+            } else {
+
+                // options.uidToken is passed in when the view is
+                // instantiated via goldstoneRouter.js
+
+                self.submitRequest(self.uidToken + '&' + $(this).serialize());
+            }
+        });
+    },
+
+    clearFields: function() {
+        // clear input fields
+        $('#password').val('');
+        $('#confirm_password').val('');
+    },
+
+    submitRequest: function(input) {
+        var self = this;
+
+        // Upon clicking the submit button, the serialized user input is sent
+        // via $.post to check the credentials. If successful, invoke "done"
+        // if not, invoke "fail"
+
+        $.post('/accounts/password/reset/confirm/', input, function() {})
+            .done(function(success) {
+
+                // clear input fields
+                self.clearFields();
+
+                // and add a success message to the top of the screen
+                goldstone.raiseInfo('Password changed. Redirecting to login.');
+
+
+                setTimeout(function() {
+                    location.href = '/login/';
+
+                }, 2000);
+
+            })
+            .fail(function(fail) {
+                // and add a message to the top of the screen that logs what
+                // is returned from the call
+                if (fail.non_field_errors) {
+                    goldstone.raiseWarning(fail.non_field_errors);
+                } else {
+                    // clear input fields
+                    self.clearFields();
+                    goldstone.raiseWarning('Password reset failed.');
+                }
+
+            });
+    }
+
+});
+;
+/**
+ * Copyright 2015 Solinea, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 var NodeReportPageView = GoldstoneBasePageView.extend({
 
     defaults: {},
@@ -9710,6 +9803,68 @@ var NodeServiceStatusView = GoldstoneBaseView.extend({
     template: _.template('<div class="alert alert-danger popup-message" hidden="true"></div>' +
         '<div class="spinnerPlace"></div>' +
         '<div class="mainContainer"></div>')
+
+});
+;
+/**
+ * Copyright 2015 Solinea, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+var PasswordResetView = GoldstoneBaseView.extend({
+
+    initialize: function(options) {
+        this.addHandlers();
+    },
+
+    addHandlers: function() {
+        var self = this;
+
+        $('.login-form').on('submit', function(e) {
+            e.preventDefault();
+            self.submitRequest($(this).serialize());
+        });
+    },
+
+    submitRequest: function(input) {
+        var self = this;
+
+        // Upon clicking the submit button, the serialized user input is sent
+        // via $.post to check the credentials. If successful, invoke "done"
+        // if not, invoke "fail"
+
+        $.post('/accounts/password/reset/', input, function() {})
+            .done(function(success) {
+
+                // and add a message to the top of the screen that logs what
+                // is returned from the call
+                goldstone.raiseInfo('Password reset instructions have been emailed to you<br>Please click the link in your email');
+            })
+            .fail(function(fail) {
+                // and add a message to the top of the screen that logs what
+                // is returned from the call
+
+                // TODO: change this after SMTP handling is set up
+                // to reflect the proper error
+                goldstone.raiseInfo(fail.responseJSON.detail);
+            });
+    },
+
+    render: function() {
+        this.$el.html(this.template());
+        return this;
+    }
 
 });
 ;
