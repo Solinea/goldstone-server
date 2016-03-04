a69ada2e5c9266e47c4a3e8661d39a418cbc02d3
Alex Jacobs <alex@solinea.com>
2016-03-04 15:37:12 -0800

    wip adds sorting to logs/api/events.

--- goldstone/static/bundle/bundle.js
+++ goldstone/static/bundle/bundle.js
@@ -6741,32 +6741,32 @@ var LogBrowserDataTableView = DataTableBaseView.extend({
             "searching": true,
             "ordering": false,
             "order": [
-                [0, 'desc']
+                [1, 'desc']
             ],
             "columnDefs": [{
                 "data": "@timestamp",
                 "type": "date",
                 "targets": 0,
-                "sortable": false,
+                "sortable": true,
                 "render": function(data, type, full, meta) {
                     return moment(data).format();
                 }
             }, {
                 "data": "syslog_severity",
                 "targets": 1,
-                "sortable": false
+                "sortable": true
             }, {
                 "data": "component",
                 "targets": 2,
-                "sortable": false
+                "sortable": true
             }, {
                 "data": "host",
                 "targets": 3,
-                "sortable": false
+                "sortable": true
             }, {
                 "data": "log_message",
                 "targets": 4,
-                "sortable": false
+                "sortable": true
             }],
             "serverSide": true,
             "ajax": {
@@ -6798,7 +6798,7 @@ var LogBrowserDataTableView = DataTableBaseView.extend({
                     }
 
                     // uncomment for ordering by column
-                    /*
+                    
                     var columnLabelHash = {
                         0: '@timestamp',
                         1: 'syslog_severity',
@@ -6810,8 +6810,8 @@ var LogBrowserDataTableView = DataTableBaseView.extend({
                         asc: '',
                         'desc': '-'
                     };
-                    settings.url = settings.url + "&ordering=" + ascDec[sortAscDesc] + columnLabelHash[sortByColumnNumber];
-                    */
+                    // settings.url = settings.url + "&ordering=" + ascDec[sortAscDesc] + columnLabelHash[sortByColumnNumber];
+                    
                 },
                 dataSrc: "results",
                 dataFilter: function(data) {
