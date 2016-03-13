9351c897ce705342b727155721457c24476cee91
Ken Pepple <ken@solinea.com>
2016-03-13 14:31:32 +0000

    add logic to after-install to determine binary name

--- rpm_packaging/after-install.sh
+++ rpm_packaging/after-install.sh
@@ -1,16 +1,23 @@
 # echo "Pulling goldstone containers"
 # $GS_PATH/goldstone/bin/docker-compose -f $GS_PATH/goldstone/docker-compose.yml pull
 
+# find pkg type
+if [ -z /usr/lib/systemd/system/goldstone-server-enterprise.service ]; then
+  PKGNAME=goldstone-server-enterprise
+else
+  PKGNAME=goldstone-server
+fi
+
 echo "*****************************************************************************"
 echo ""
 echo " Modify $GS_PATH/goldstone/config/goldstone-prod.env"
-echo " before starting goldstone-server. See $GS_PATH/goldstone/INSTALL.md"
+echo " before starting $PKGNAME. See $GS_PATH/goldstone/INSTALL.md"
 echo " for details."
 echo ""
-echo " To enable and start goldstone-server, run:"
+echo " To enable and start $PKGNAME, run:"
 echo ""
-echo "     systemctl enable goldstone-server"
-echo "     systemctl start goldstone-server"
+echo "     systemctl enable $PKGNAME"
+echo "     systemctl start $PKGNAME"
 echo ""
 echo "*****************************************************************************"
 
