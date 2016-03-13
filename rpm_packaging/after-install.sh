b496e4074d2a5e288e42bbe1e962d23e9a1e96dd
Ken Pepple <ken@solinea.com>
2016-03-13 15:29:01 +0000

    document enterprise login requirements for rpm. fixes issue #385.

--- rpm_packaging/after-install.sh
+++ rpm_packaging/after-install.sh
@@ -19,6 +19,17 @@ echo ""
 echo "     systemctl enable $PKGNAME"
 echo "     systemctl start $PKGNAME"
 echo ""
+
+if [ $PKGNAME="goldstone-server-enterprise" ]; then
+  echo " Before starting $PKGNAME you must set your Goldstone license variables."
+  echo " You can do this via the following commands:     "
+  echo "      # export GOLDSTONE_REPO_USER=<<YOUR_REPO_USER>> "
+  echo "      # export GOLDSTONE_REPO_PASS=<<YOUR_REPO_PASS>>"
+  echo "      # export GOLDSTONE_REPO_EMAIL=<<YOUR_REPO_EMAIL>>"
+  echo ""
+  echo "Please contact your Goldstone sales rep is you have any questions."
+fi
+
 echo "*****************************************************************************"
 
 # restart the syslog daemon (assumes systemd)
