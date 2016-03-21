5d761b33ad81da32c6eb1ee4f4b6934c55e7671e
Ken Pepple <ken@solinea.com>
2016-03-20 19:53:27 -0700

    backout double container removes in rpm remove scripts

--- rpm_packaging/before-remove.sh
+++ rpm_packaging/before-remove.sh
@@ -13,18 +13,15 @@ systemctl disable $PKGNAME
 echo "Stopping containers..."
 if [ `docker ps | grep goldstone | wc -l` -gt 0 ]; then
   docker stop -t 15 $(docker ps -a | grep goldstone | awk '{print $1}' | xargs) || /bin/true
-  docker stop -t 15 $(docker ps -a | grep goldstone | awk '{print $1}' | xargs) || /bin/true
 fi
 
 # remove stopped containers
 echo "Removing containers..."
 if [ `docker ps -a | grep goldstone | wc -l` -gt 0 ]; then
   docker rm -f $(docker ps -a | grep goldstone | awk '{print $1}' | xargs) || /bin/true
-  docker rm -f $(docker ps -a | grep goldstone | awk '{print $1}' | xargs) || /bin/true
 fi
 
 echo "Removing images..."
 if [ `docker images -a | grep goldstone | wc -l` -gt 0 ]; then
-  docker rmi -f $(docker images | grep goldstone | awk '{print $3}' | xargs) || /bin/true
   docker rmi -f $(docker images | grep goldstone | awk '{print $3}' | xargs) || /bin/true
 fi
