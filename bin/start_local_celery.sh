99b797aabf7c68cbb3b98c35db0b7d01335283bf
John Stanford <jxstanford@gmail.com>
2016-03-31 16:33:16 -0700

    start_local_env.sh should does the right thing

--- bin/start_local_celery.sh
+++ bin/start_local_celery.sh
@@ -1,18 +1,30 @@
 #!/bin/bash
 
-cd ${PROJECT_HOME}/goldstone-server
-. docker/config/goldstone-dev.env
-cat docker/config/goldstone-dev.env | sed -e '/^[#]/d' -e '/^$/d' | cut -f1 -d'=' | while read v 
-do  
+#
+# load up the standard goldstone environment vars
+#
+
+TOP_DIR=${GS_PROJ_TOP_DIR:-${PROJECT_HOME}/goldstone-server}
+cd $TOP_DIR 
+
+
+cat docker/config/goldstone-dev.env | sed -e '/^[#]/d' -e '/^$/d' > /var/tmp/gsenv
+while read v; do 
     export $v
-done
+done < /var/tmp/gsenv
+rm /var/tmp/gsenv
+
+
 export ENVDIR=${HOME}/.virtualenvs/goldstone-server
 export APPDIR=${ENVDIR}
-export GS_DB_HOST=127.0.0.1
 export DJANGO_SETTINGS_MODULE=goldstone.settings.local_dev
+export GOLDSTONE_REDIS_HOST=127.0.0.1
 export GS_LOCAL_DEV=true
-export GS_DEV_DJANGO_PORT=8001
-export GS_START_RUNSERVER=false
-export CELERY_BG=false
-. docker/goldstone-base/docker-entrypoint.sh
+python manage.py migrate --noinput
+python manage.py loaddata $(find goldstone -regex '.*/fixtures/.*' | xargs)
+python manage.py collectstatic  --noinput
+python post_install.py
+exec celery worker --app goldstone --queues default --beat --purge \
+            --workdir ${TOP_DIR} --config ${DJANGO_SETTINGS_MODULE} \
+            --without-heartbeat --loglevel=${CELERY_LOGLEVEL} -s /var/tmp/celerybeat-schedule
 
