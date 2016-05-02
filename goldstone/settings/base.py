b59fa4b80c45e1a4a3ea54a8755921c387d10aec
John Stanford <jxstanford@gmail.com>
2016-05-02 16:43:20 -0700

    bumped images

--- goldstone/settings/base.py
+++ goldstone/settings/base.py
@@ -24,7 +24,7 @@ from celery.schedules import crontab
 from kombu import Exchange, Queue
 
 # this version should be managed by the bump_version.sh script
-GOLDSTONE_VERSION = '1.1.1-SNAPSHOT.185.g3787d39.logstash.bugs'
+GOLDSTONE_VERSION = '1.1.1-SNAPSHOT.201.gc3e4b3b.develop'
 
 CURRENT_DIR = os.path.dirname(__file__)
 TEMPLATE_DIRS = (os.path.join(CURRENT_DIR, '../templates'),)
