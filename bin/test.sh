a8eb45bc4237a436bcad2e0e3f1a6a9b2babdb68
John Stanford <jxstanford@gmail.com>
2016-05-03 16:57:42 -0700

    moved nova_hypervisor_stats task back to gs-oss

--- bin/test.sh
+++ bin/test.sh
@@ -1,8 +1,20 @@
 #!/bin/bash
 
-docker exec -it goldstoneserver_gsapp_1 bash -i -c \
-    "coverage run --branch --source='./goldstone' --omit='./goldstone/settings/*,*/test*' manage.py test $@ \
-    && coverage html \
-    && coverage report" 
-pep8
+docker ps | grep goldstoneserver_gsapp_1 > /dev/null 2>&1
+RC=$?
+
+if [[ $RC != 0 ]] ; then
+    # try to run locally
+    coverage run --branch --source='./goldstone' --omit='./goldstone/settings/*,*/test*' manage.py test $@ \
+        && coverage html \
+        && coverage report \
+        && pep8 
+else
+    # try to run in the docker container
+    docker exec -it goldstoneserver_gsapp_1 bash -i -c \
+        "coverage run --branch --source='./goldstone' --omit='./goldstone/settings/*,*/test*' manage.py test $@ \
+        && coverage html \
+        && coverage report \
+        && pep8" 
+fi
 
