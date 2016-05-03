db3a98d038460ca6d94f8f3a17f3348061c8e7f4
John Stanford <jxstanford@gmail.com>
2016-05-03 08:49:39 -0700

    starting v1.2.0

--- goldstone/settings/base.py
+++ goldstone/settings/base.py
@@ -24,7 +24,7 @@ from celery.schedules import crontab
 from kombu import Exchange, Queue
 
 # this version should be managed by the bump_version.sh script
-GOLDSTONE_VERSION = '1.1.1'
+GOLDSTONE_VERSION = '1.1.1-SNAPSHOT.203.g74b6065.develop'
 
 CURRENT_DIR = os.path.dirname(__file__)
 TEMPLATE_DIRS = (os.path.join(CURRENT_DIR, '../templates'),)
