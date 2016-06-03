b1966f5f2dd9242c33551dc6aa2cda83dd2c37b9
John Stanford <jxstanford@gmail.com>
2016-06-03 16:16:54 -0700

    adjusted RPM output order to be more clear

--- rpm_packaging/after-install.sh
+++ rpm_packaging/after-install.sh
@@ -11,6 +11,18 @@ export GS_PATH="/opt"
 
 
 echo "*****************************************************************************"
+if [ "$PKGNAME" = "goldstone-server-enterprise" ]; then
+  echo ""
+  echo " Before starting $PKGNAME you must set your Goldstone license variables."
+  echo " and login into the custom repository."
+  echo " You can do this via the following commands:        "
+  echo "   # `tput setaf 6`export GOLDSTONE_REPO_USER=<<YOUR_REPO_USER>>`tput sgr0`  "
+  echo "   # `tput setaf 6`export GOLDSTONE_REPO_PASS=<<YOUR_REPO_PASS>>`tput sgr0`  "
+  echo "   # `tput setaf 6`docker login -u \$GOLDSTONE_REPO_USER -p \$GOLDSTONE_REPO_PASS gs-docker-ent.bintray.io`tput sgr0`"
+  echo ""
+  echo "Please contact `tput setaf 2`support@solinea.com`tput sgr0` if you have any questions."
+fi
+
 echo ""
 echo " `tput setaf 6`Modify $GS_PATH/goldstone/config/goldstone-prod.env`tput sgr0`"
 echo " before starting $PKGNAME. See $GS_PATH/goldstone/INSTALL.md"
@@ -22,16 +34,6 @@ echo "     `tput setaf 6`systemctl enable $PKGNAME`tput sgr0`"
 echo "     `tput setaf 6`systemctl start $PKGNAME`tput sgr0`"
 echo ""
 
-if [ "$PKGNAME" = "goldstone-server-enterprise" ]; then
-  echo " Before starting $PKGNAME you must set your Goldstone license variables."
-  echo " and login into the custom repository."
-  echo " You can do this via the following commands:        "
-  echo "   # `tput setaf 6`export GOLDSTONE_REPO_USER=<<YOUR_REPO_USER>>`tput sgr0`  "
-  echo "   # `tput setaf 6`export GOLDSTONE_REPO_PASS=<<YOUR_REPO_PASS>>`tput sgr0`  "
-  echo "   # `tput setaf 6`docker login -u \$GOLDSTONE_REPO_USER -p \$GOLDSTONE_REPO_PASS gs-docker-ent.bintray.io`tput sgr0`"
-  echo ""
-  echo "Please contact `tput setaf 2`support@solinea.com`tput sgr0` if you have any questions."
-fi
 
 echo "*****************************************************************************"
 
